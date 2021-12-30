f1354909f96786810bb11d51c4705a234e4db256
Peter Wawa <meelisn@outlook.com>
2021-12-30 18:52:47 +0200

    Make one measure block for all tests

--- src/Performance/Get-Service.benchmark.ps1
+++ src/Performance/Get-Service.benchmark.ps1
@@ -9,41 +9,43 @@ param (
 
 Add-Type -AssemblyName System.ServiceProcess
 
+$Technique = @{
+    'Get-Service | Where' = {
+        $null = Get-Service | Where-Object { $_.Name -like "Bits" }
+    }
+    'Get-Service'         = {
+        $null = Get-Service "Bits"
+    }
+    '.NET | Where'        = {
+        $null = [ServiceProcess.ServiceController]::GetServices() | Where-Object { $_.Name -like 'bits' }
+    }
+    '.NET direct'         = {
+        $null = [ServiceProcess.ServiceController] @{ Name = 'bits' }
+    }
+}
+
+if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
+    $Technique += @{
+        'GCIM'      = {
+            $null = Get-CimInstance -ClassName Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
+        }
+        'GCIM full' = {
+            $null = Get-CimInstance -ClassName Win32_Service -Filter "Name = 'BITS'"
+        }
+    }
+}
+
+if ($PSVersionTable.PSVersion.Major -le 5) {
+    $Technique += @{
+        'GWMI'      = {
+            $null = Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
+        }
+        'GWMI full' = {
+            $null = Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'"
+        }
+    }
+}
+
 for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $iterations -Technique @{
-        'Get-Service | Where' = {
-            $null = Get-Service | Where-Object { $_.Name -like "Bits" }
-        }
-        'Get-Service'         = {
-            $null = Get-Service "Bits"
-        }
-        '.NET | Where'        = {
-            $null = [ServiceProcess.ServiceController]::GetServices() | Where-Object { $_.Name -like 'bits' }
-        }
-        '.NET direct'         = {
-            $null = [ServiceProcess.ServiceController] @{ Name = 'bits' }
-        }
-    } -GroupName ('PS all: {0} times' -f $iterations)
-
-    if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
-        Measure-Benchmark -RepeatCount $iterations -Technique @{
-            'GCIM'      = {
-                $null = Get-CimInstance -ClassName Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
-            }
-            'GCIM full' = {
-                $null = Get-CimInstance -ClassName Win32_Service -Filter "Name = 'BITS'"
-            }
-        } -GroupName ('PS > 2: {0} times' -f $iterations)
-    }
-
-    if ($PSVersionTable.PSVersion.Major -le 5) {
-        Measure-Benchmark -RepeatCount $iterations -Technique @{
-            'GWMI'      = {
-                $null = Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
-            }
-            'GWMI full' = {
-                $null = Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'"
-            }
-        } -GroupName ('PS < 6: {0} times' -f $iterations)
-    }
+    Measure-Benchmark -RepeatCount $iterations -Technique $Technique -GroupName ('{0} times' -f $iterations)
 }
