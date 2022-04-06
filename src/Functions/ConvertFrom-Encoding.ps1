23379e4c3c02807552869f26ec28f80cf257f7aa
Peter Wawa <meelisn@outlook.com>
2022-04-06 15:27:30 +0300

    add encoding conversion functions

---
+++ src/Functions/ConvertFrom-Encoding.ps1
@@ -0,0 +1,21 @@
+﻿function ConvertFrom-Encoding {
+    param (
+            [parameter(
+                Mandatory,
+                ValueFromPipeline
+            )]
+            [string]
+        $Text,
+            [Text.Encoding]
+        $From = [Text.Encoding]::UTF8
+    )
+    begin {
+        $to = [Text.Encoding]::Unicode
+    }
+    process {
+        $bytes = [byte[]]([char[]]$text)
+        $NewBytes = [Text.Encoding]::convert($From, $to, $bytes)
+        Write-Verbose -Message ($NewBytes -join ' ')
+        $to.GetString($NewBytes)
+    }
+}
---
+++ src/Functions/ConvertTo-Encoding.ps1
@@ -0,0 +1,22 @@
+﻿function ConvertTo-Encoding {
+    param (
+            [parameter(
+                Mandatory,
+                ValueFromPipeline
+            )]
+            [string]
+        $Text,
+            [Text.Encoding]
+        $To = [Text.Encoding]::UTF8
+    )
+    begin {
+        $From = [Text.Encoding]::Unicode
+    }
+
+    process {
+        $bytes = $from.GetBytes($Text)
+        $NewBytes = [Text.Encoding]::convert($From, $To, $bytes)
+        Write-Verbose -Message ($NewBytes -join ' ')
+        [char[]]$NewBytes -join ''
+    }
+}
