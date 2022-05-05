1c7ff7374ab94ec29421d3e7dd9a694811ef4af2
Peter Wawa <meelisn@outlook.com>
2022-05-05 22:42:15 +0300

    add DNS Client event log query sample

---
+++ src/Snippets/Get-DnsClientEvent.ps1
@@ -0,0 +1,25 @@
+[CmdletBinding()]
+param (
+        [string]
+    $QueryName = 'rdm.et.ee'
+)
+Get-WinEvent -FilterHashtable @{
+    LogName   = 'Microsoft-Windows-DNS-Client/Operational'
+    Id        = 3008
+    StartTime = [datetime]::Now.AddHours(-1)
+    Data      = $QueryName
+} | ForEach-Object {
+    $xmlevent = [xml]$_.ToXml()
+    $ProcessId = $xmlevent.event.System.Execution.ProcessId
+    $Process = Get-Process -id $ProcessId
+    $ParentProcess = Get-Process -Id (
+        Get-CimInstance Win32_Process -Filter "ProcessId = $ProcessId"
+    ).ParentProcessId
+    [pscustomObject] @{
+        TimeCreated = $_.TimeCreated
+        QueryName   = $xmlevent.SelectNodes('//*[@Name = "QueryName"]').InnerText
+        Process     = $Process.Name
+        ProcessPath = $Process.Path
+        Parent      = $ParentProcess.Name
+    }
+}
