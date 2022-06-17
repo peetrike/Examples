a93f3a0600ce2269b63260d08ba9b28d648de759
Peter Wawa <meelisn@outlook.com>
2022-06-17 10:31:10 +0300

    Add sample using Show-Command as GUI element

---
+++ src/Gui/Test-Command.ps1
@@ -0,0 +1,19 @@
+﻿<#
+    .SYNOPSIS
+        Demonstrates Show-Command in the script.
+    .DESCRIPTION
+        This script demonstrates the use of the Show-Command in the script.
+#>
+
+function test-me {
+    param (
+            [string]
+        $One = 'One',
+            [string]
+        $Two
+    )
+    $PSBoundParameters
+}
+
+Show-Command -Name test-me -PassThru | Invoke-Expression
+Show-Command -Name test-me -ErrorPopup
