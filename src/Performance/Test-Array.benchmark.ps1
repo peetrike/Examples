6259b2989b35c327fbc567d111f0c5daaac7ad94
Peter Wawa <meelisn@outlook.com>
2022-09-19 12:29:57 +0300

    Add local $Iterations value in test scriptblocks

--- src/Performance/Test-Array.benchmark.ps1
+++ src/Performance/Test-Array.benchmark.ps1
@@ -1,4 +1,5 @@
-﻿#Requires -Module BenchPress
+﻿#Requires -Version 3
+#Requires -Modules BenchPress
 
 param (
     $Min = 10,
@@ -11,30 +12,35 @@ for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
     Measure-Benchmark -RepeatCount $Repeat -Technique @{
         'Array += in loop'   = {
             $Array = @()
+            $Iterations = $Iterations
             1..$Iterations | ForEach-Object {
                 $Array += 'tere'
             }
         }
         'Array assignment'   = {
             $Array = @()
+            $Iterations = $Iterations
             $Array = 1..$Iterations | ForEach-Object {
                 'tere'
             }
         }
         'ArrayList'          = {
             $Array = [Collections.ArrayList] @()
+            $Iterations = $Iterations
             1..$Iterations | ForEach-Object {
                 [void] $Array.Add('tere')
             }
         }
         'Generic list'       = {
             $Array = [Collections.Generic.List[string]] @()
+            $Iterations = $Iterations
             1..$Iterations | ForEach-Object {
                 $Array.Add('tere')
             }
         }
         'Generic Collection' = {
             $Array = [Collections.ObjectModel.Collection[string]] @()
+            $Iterations = $Iterations
             1..$Iterations | ForEach-Object {
                 $Array.Add('tere')
             }
