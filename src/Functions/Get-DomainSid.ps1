bb574508d844dba245dd5b880678bb1b22e5b352
Peter Wawa <meelisn@outlook.com>
2022-11-08 18:37:53 +0200

    add new function

---
+++ src/Functions/Get-DomainSid.ps1
@@ -0,0 +1,13 @@
+﻿
+function Get-DomainSid {
+    [CmdletBinding()]
+    [OutputType([Security.Principal.SecurityIdentifier])]
+    param ()
+    $Domain = [DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+    New-Object -TypeName Security.Principal.SecurityIdentifier -ArgumentList (
+        $Domain.GetDirectoryEntry().objectSID[0],
+        0
+    )
+}
+
+Get-DomainSid
