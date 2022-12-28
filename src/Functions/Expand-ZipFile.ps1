12d71a98ba8e7591504c66863f4bf4b7624c6ba9
Peter Wawa <meelisn@outlook.com>
2022-12-28 08:53:18 +0200

    add new functions

---
+++ src/Functions/Expand-ZipFile.ps1
@@ -0,0 +1,49 @@
+﻿function Expand-ZipFile {
+    [CmdletBinding()]
+    param (
+            [parameter(
+                Mandatory = $true
+            )]
+            [ValidateScript({
+                if (Test-Path -Path $_) { $true } else {
+                    throw [Management.Automation.ItemNotFoundException] ('Path not found: {0}' -f $_)
+                }
+            })]
+        $Path,
+            [string]
+        $DestinationPath,
+            [switch]
+        $Force
+    )
+
+    if (-not $DestinationPath) {
+        $DestinationPath = $Path -replace '.zip', ''
+    }
+
+    Write-Verbose -Message ('Extract target: {0}' -f $DestinationPath)
+    if (Test-Path -Path $DestinationPath -PathType Container) {
+        if ($Force) {
+            Write-Verbose -Message "Removing folder $DestinationPath"
+            Remove-Item -Path $DestinationPath -Force -Recurse
+        } else {
+            throw "Folder $DestinationPath already exists"
+        }
+    }
+
+    if ($PSVersionTable.PSVersion.Major -ge 5) {
+        Expand-Archive -Path $Path -DestinationPath $DestinationPath -Force:$Force
+    } else {
+        Write-Verbose -Message 'Extracting using Windows Shell'
+        $shell = New-Object -ComObject Shell.Application
+        $ArchiveItem = Get-Item -Path $Path
+        if (Test-Path -Path $DestinationPath -PathType Container) {
+            $DestinationItem = Get-Item -Path $DestinationPath
+        } else {
+            $DestinationItem = New-Item -Path $DestinationPath -ItemType Directory
+        }
+        $ShellArchive = $shell.Namespace($ArchiveItem.FullName)
+        foreach ($item in $ShellArchive.Items()) {
+            $shell.Namespace($DestinationItem.FullName).CopyHere($item)
+        }
+    }
+}
---
+++ src/Functions/Invoke-BitsDownload.ps1
@@ -0,0 +1,84 @@
+﻿#Requires -Version 2.0
+#Requires -Modules BitsTransfer
+
+function Invoke-BitsDownload {
+    <#
+        .DESCRIPTION
+            Download files using BITS
+        .EXAMPLE
+            Invoke-BitsDownload 'https://server/path/file.zip'
+
+            Downloads given URL to default destination (user TEMP directory)
+        .EXAMPLE
+            Invoke-BitsDownload -Url https://server/path/file.zip -Destination c:\downloads
+
+            Downloads given URL to specified destination.
+    #>
+
+    [CmdletBinding(
+        SupportsShouldProcess = $true
+    )]
+    param (
+            [Parameter(
+                Mandatory = $true,
+                ValueFromPipelineByPropertyName = $true,
+                Position = 0
+            )]
+            [uri]
+        $Url,
+            [ValidateScript( {
+                Test-path -Path $_ -PathType Container
+            })]
+            [string]
+        $Destination = $env:TEMP,
+            [switch]
+        $Force
+    )
+
+    process {
+        $DownloadFileName = $Url.Segments[-1]
+        $DownloadFullPath = Join-Path -Path $Destination -ChildPath $DownloadFileName
+
+        if (Test-Path -Path $DownloadFullPath -PathType Leaf) {
+            if ($Force.IsPresent) {
+                Remove-Item -Path $DownloadFullPath -Force
+            } else {
+                Write-Warning -Message ('File {0} already exists' -f $DownloadfullPath)
+                return
+            }
+        }
+
+        $JobProps = @{
+            Source        = $Url.AbsoluteUri
+            Destination   = $Destination
+            RetryInterval = 61
+            DisplayName   = $DownloadFileName
+            Asynchronous  = $true
+        }
+        $bitsjob = Start-BitsTransfer @JobProps
+        Write-Verbose -Message ('Downloading {0}' -f $DownloadFileName)
+
+        while ('Transferred', 'Error' -notcontains $bitsjob.JobState) {
+            Write-Verbose -Message (
+                'Downloading state: {0}; {1} Bytes' -f $bitsjob.JobState, $bitsjob.BytesTransferred
+            )
+
+            Start-Sleep -Seconds 3
+            if ($bitsjob.JobState -eq "TransientError" -and $bitsjob.TransientErrorCount -lt 5) {
+                $null = Resume-BitsTransfer -BitsJob $bitsjob -Asynchronous
+            } elseif ($bitsjob.JobState -eq "TransientError") {
+                Remove-BitsTransfer -BitsJob $bitsjob
+                Write-Error -Message ('Transient error downloading from: {0}' -f $Url.OriginalString)
+                return
+            }
+        }
+        switch ($bitsjob.JobState) {
+            'Transferred' {
+                Complete-BitsTransfer -BitsJob $bitsjob
+            }
+            'Error' {
+                Write-Error -Message 'An error occurred'
+            }
+        }
+    }
+}
