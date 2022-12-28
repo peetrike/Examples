6a205993e10876aa2f2f92bb38e960ab3fefbab5
Peter Wawa <meelisn@outlook.com>
2022-12-28 08:51:15 +0200

    add possibility to save output to file

--- src/Scripts/Test-scriptstart.ps1
+++ src/Scripts/Test-scriptstart.ps1
@@ -5,16 +5,28 @@
 )]
 param (
         [switch]
-    $Param1,
+    $ToFile,
     $Param2 = 'Parameeter'
 )
 
-'$PSScriptRoot = {0}' -f $PSScriptRoot
-'$PSCommandPath = {0}' -f $PSCommandPath
-'Source: {0}' -f $MyInvocation.MyCommand.Source
-'My Invocation Path: {0}' -f $MyInvocation.MyCommand.Path
-'My Invocation name: {0}' -f $MyInvocation.MyCommand.Name
-'ParameterSet name: {0}' -f $PsCmdlet.ParameterSetName
+$result = @(
+    '$PWD = {0}' -f $PWD
+    '.NET current directory = {0}' -f [Environment]::CurrentDirectory
+    'Home Folder: {0}' -f (Get-PSProvider -PSProvider FileSystem).Home
+    '$PSScriptRoot = {0}' -f $PSScriptRoot
+    '$PSCommandPath = {0}' -f $PSCommandPath
+    'Source: {0}' -f $MyInvocation.MyCommand.Source
+    'My Invocation Path: {0}' -f $MyInvocation.MyCommand.Path
+    'My Invocation name: {0}' -f $MyInvocation.MyCommand.Name
+    'ParameterSet name: {0}' -f $PsCmdlet.ParameterSetName
 
-'Bound Parameters:'
-$PSBoundParameters
+    'Bound Parameters:'
+    $PSBoundParameters
+)
+
+if ($ToFile) {
+    $FilePath = Join-Path -Path $PWD -ChildPath 'tulemus.txt'
+    Set-Content -Path $FilePath -Value $result
+} else {
+    $result
+}
