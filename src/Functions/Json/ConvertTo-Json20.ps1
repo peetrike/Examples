1588ff8b2d52068b7dfbb9d537bbc7c7592b7400
Peter Wawa <meelisn@outlook.com>
2022-12-29 13:10:23 +0200

    Refactor script
    - Change order of replacements in EscapeJson function
    - Modify comment-based help

--- src/Functions/Json/ConvertTo-Json20.ps1
+++ src/Functions/Json/ConvertTo-Json20.ps1
@@ -8,7 +8,7 @@
     .NOTES
         Original author: Joakim Borger Svendsen - https://github.com/EliteLoser/ConvertTo-Json
     .LINK
-        https://www.json.org
+        https://www.rfc-editor.org/info/std90
     .EXAMPLE
         Get-ChildItem | ConvertTo-Json20 -Depth 1
 
@@ -28,13 +28,8 @@ function EscapeJson {
         $String
     )
     # This is returned
-    $String -replace '\\',
-        '\\' -replace '\n',
-        '\n' -replace '\u0008',
-        '\b' -replace '\u000C',
-        '\f' -replace '\r',
-        '\r' -replace '\t',
-        '\t' -replace '"', '\"'    # removed: -replace '/', '\/'
+    $String -replace '\\', '\\' -replace '"', '\"' -replace '\u0008', '\b' -replace '\u000C',
+        '\f' -replace '\n', '\n' -replace '\r', '\r' -replace '\t', '\t'    # removed: -replace '/', '\/'
 }
 
 function ConvertToJsonInternal {
@@ -164,12 +159,21 @@ function ConvertTo-Json20 {
             ConvertTo-Json for PowerShell 2.0
         .DESCRIPTION
             This script contains function ConvertTo-Json20 which is written for PowerShell 2.0
-        .LINK
-            https://learn.microsoft.com/powershell/module/microsoft.powershell.utility/convertto-json
         .EXAMPLE
             Get-ChildItem | ConvertTo-Json20 -Depth 1
 
             This example converts directory listing to JSON format, expanding only 1 level of objects
+        .EXAMPLE
+            @{Account="User01";Domain="Domain01";Admin="True"} | ConvertTo-Json20 -Compress
+
+            This command shows the effect of using the Compress parameter of ConvertTo-Json20.
+            The compression affects only the appearance of the string, not its validity.
+        .EXAMPLE
+            ConvertTo-Json20 -InputObject (Get-Date) -DateAsIso
+
+            This command converts date into ISO 8601 format.
+        .LINK
+            https://learn.microsoft.com/powershell/module/microsoft.powershell.utility/convertto-json
     #>
 
     [OutputType([String])]
@@ -180,16 +184,32 @@ function ConvertTo-Json20 {
                 ValueFromPipeline = $true
             )]
             [AllowNull()]
+            # Specifies the objects to convert to JSON format. Enter a variable that contains the objects,
+            # or type a command or expression that gets the objects.
+            # You can also pipe an object to `ConvertTo-Json`.
+
+            # The InputObject parameter is required, but its value can be null (`$null`) or an empty string.
+            # When the input object is `$null`, `ConvertTo-Json` returns the JSON representation of `null`.
+            # When the input object is an empty string, `ConvertTo-Json` returns the JSON representation of an
+            # empty string.
         $InputObject,
+
             [ValidateRange(1, 100)]
             [int]
+            # Specifies how many levels of contained objects are included in the JSON representation.
+            # The value can be any number from `1` to `100`. The default value is `2`.
+            # `ConvertTo-Json` emits a warning if the number of levels in an input object exceeds this number.
         $Depth = 2,
+
             [switch]
+            # Omits white space and indented formatting in the output string.
         $Compress,
             [Alias('IsoDate')]
             [switch]
+            # Converts DateTime object to ISO 8601 format.
         $DateAsIso,
             [switch]
+            # Converts all enumerations to their string representation.
         $EnumsAsStrings
     )
 
