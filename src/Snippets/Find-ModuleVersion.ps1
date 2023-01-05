11e2795b87b2f2f86ad8a3887ee158c1cf6ca96a
Peter Wawa <meelisn@outlook.com>
2023-01-05 15:54:46 +0200

    Make script work with PowerShell 7
    also ensure, that PowerShellGet v3 is not loaded

--- src/Snippets/Find-ModuleVersion.ps1
+++ src/Snippets/Find-ModuleVersion.ps1
@@ -1,4 +1,4 @@
-#Requires -Modules PowerShellGet
+#Requires -Modules @{ModuleName = 'PowerShellGet'; MaximumVersion = 2.99}
 <#
     .SYNOPSIS
         Find local modules that have more than 2 versions on disk
@@ -9,7 +9,9 @@
 [CmdletBinding()]
 param (
         [ValidateSet('AllUsers', 'CurrentUser')]
-    $Scope = 'AllUsers'
+    $Scope = 'AllUsers',
+        [ValidateRange(1, 10)]
+    $VersionCount = 2
 )
 
 $PathName = $Scope + 'Modules'
@@ -17,5 +19,5 @@ $PathName = $Scope + 'Modules'
 $ModulePath = $PSGetPath.$PathName
 
 Get-ChildItem -Path $ModulePath\* -Include * -Directory |
-    Group-Object Parent |
-    Where-Object Count -gt 2
+    Group-Object { $_.Parent.Name } |
+    Where-Object Count -gt $VersionCount
