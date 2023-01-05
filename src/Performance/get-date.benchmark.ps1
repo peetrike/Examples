c28d9dc436ab402a27ed51fe9bddc66d682b9541
Peter Wawa <meelisn@outlook.com>
2023-01-05 08:22:49 +0200

    reorder tests

--- src/Performance/get-date.benchmark.ps1
+++ src/Performance/get-date.benchmark.ps1
@@ -7,24 +7,24 @@ param (
 
 for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
     Measure-Benchmark -RepeatCount $iterations -Technique @{
-        '.Net specific'     = { '{0:HH\:mm\:ss}' -f [datetime]::Now }
         '.Net generic'      = { '{0:T}' -f [datetime]::Now }
-        'ToString specific' = { [datetime]::Now.ToString('HH\:mm\:ss') }
+        '.Net specific'     = { '{0:HH\:mm\:ss}' -f [datetime]::Now }
         'ToString generic'  = { [datetime]::Now.ToString('T') }
-        'Format specific'   = { Get-Date -Format 'HH:mm:ss' }
+        'ToString specific' = { [datetime]::Now.ToString('HH:mm:ss') }
         'Format generic'    = { Get-Date -Format T }
-        'Format Unix s'     = { Get-Date -UFormat '%H:%M:%S' }
+        'Format specific'   = { Get-Date -Format 'HH:mm:ss' }
         'Format Unix g'     = { Get-Date -UFormat '%T' }
+        'Format Unix s'     = { Get-Date -UFormat '%H:%M:%S' }
         'DisplayHint'       = { Get-Date -DisplayHint Time }
     } -GroupName ('Time only: {0} times' -f $iterations)
 }
 
 for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
     Measure-Benchmark -RepeatCount $iterations -Technique @{
-        '.Net now'          = { '{0:dd.MM.yyyy}' -f [datetime]::Now }
-        '.Net today'        = { '{0:dd.MM.yyyy}' -f [datetime]::Today }
-        'ToString specific' = { [datetime]::Now.ToString('dd.MM.yyyy') }
-        'ToString generic'  = { [datetime]::Now.ToString('d') }
+        '.Net today'        = { '{0:d}' -f [datetime]::Today }
+        '.Net now'          = { '{0:d}' -f [datetime]::Now }
+        'ToString specific' = { [datetime]::Today.ToString('dd.MM.yyyy') }
+        'ToString generic'  = { [datetime]::Today.ToString('d') }
         'Format specific'   = { Get-Date -Format 'dd.MM.yyyy' }
         'Format generic'    = { Get-Date -Format d }
         'Format Unix s'     = { Get-Date -UFormat '%d.%m.%Y' }
