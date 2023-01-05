5712b422fb50b856283129c8bbbcfe585679355f
Peter Wawa <meelisn@outlook.com>
2023-01-05 08:23:21 +0200

    add tests for PowerShell 2

--- src/Performance/Get-Process.benchmark.ps1
+++ src/Performance/Get-Process.benchmark.ps1
@@ -1,4 +1,4 @@
-﻿#Requires -Module BenchPress
+﻿# Requires -Module BenchPress
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCmdLets', 'Get-CimInstance')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWMICmdlet', '')]
@@ -7,37 +7,49 @@ param (
     $Max = 10
 )
 
-$Technique = @{
-    'Get-Process' = {
-        $null = (Get-Process -id $PID).Name
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    $Technique = @{
+        'Get-Process' = {
+            $null = (Get-Process -id $PID).Name
+        }
+        '.NET direct' = {
+            $null = [Diagnostics.Process]::GetCurrentProcess().Name
+        }
     }
-    '.NET direct' = {
-        $null = [Diagnostics.Process]::GetCurrentProcess().Name
-    }
-}
 
-if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
-    $Technique += @{
-        'GCIM'      = {
-            $null = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
-        }
-        'GCIM full' = {
-            $null = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID").Name
-        }
-    }
-}
-
-if ($PSVersionTable.PSVersion.Major -le 5) {
-    $Technique += @{
-        'GWMI'      = {
-            $null = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
-        }
-        'GWMI full' = {
-            $null = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID").Name
+    if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
+        $Technique += @{
+            'GCIM'      = {
+                $null = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
+            }
+            'GCIM full' = {
+                $null = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID").Name
+            }
         }
     }
-}
 
-for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $iterations -Technique $Technique -GroupName ('{0} times' -f $iterations)
+    if ($PSVersionTable.PSVersion.Major -le 5) {
+        $Technique += @{
+            'GWMI'      = {
+                $null = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
+            }
+            'GWMI full' = {
+                $null = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID").Name
+            }
+        }
+    }
+
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        Measure-Benchmark -RepeatCount $iterations -Technique $Technique -GroupName ('{0} times' -f $iterations)
+    }
+} else {
+    Write-Verbose -Message 'PowerShell 2'
+    Import-Module .\measure.psm1
+    Measure-ScriptBlock -Method '.NET' -Iterations $Max -ScriptBlock {
+        [Diagnostics.Process]::GetCurrentProcess().Name
+    }
+    Measure-ScriptBlock -Method 'cmdlet' -Iterations $Max -ScriptBlock { (Get-Process -id $PID).Name }
+    Measure-ScriptBlock -Method 'WMI' -Iterations $Max -ScriptBlock {
+        (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
+    }
 }
