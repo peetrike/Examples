68b63d0281cde1ca08cf1051e5197dfd8cf63297
Peter Wawa <meelisn@outlook.com>
2023-01-10 14:54:48 +0200

    Add PowerShell 2.0 tests

--- src/Performance/Creating objects.benchmark.ps1
+++ src/Performance/Creating objects.benchmark.ps1
@@ -1,5 +1,5 @@
-﻿#Requires -Version 3.0
-#Requires -Module BenchPress
+﻿# Requires -Version 3.0
+# Requires -Module BenchPress
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'list')]
 param (
@@ -7,22 +7,39 @@ param (
     $Max = 100000
 )
 
-for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $Iterations -Technique @{
-        'New-Object'              = {
-            $list = New-Object System.Collections.Generic.List[Object]
-        }
-        '5.1 - [object]::new()'   = {
-            $list = [Collections.Generic.List[Object]]::new()
-        }
-        'casting empty array'     = {
-            $list = [Collections.Generic.List[Object]]@()
-        }
-        'casting empty hashtable' = {
-            $list = [Collections.Generic.List[Object]]@{}
-        }
-        'strong typing'           = {
-            [Collections.Generic.List[Object]] $list = @()
-        }
-    } -GroupName $Iterations
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        Measure-Benchmark -RepeatCount $Iterations -Technique @{
+            'New-Object'              = {
+                $list = New-Object System.Collections.Generic.List[Object]
+            }
+            '5.1 - [object]::new()'   = {
+                $list = [Collections.Generic.List[Object]]::new()
+            }
+            'casting empty array'     = {
+                $list = [Collections.Generic.List[Object]] @()
+            }
+            'casting empty hashtable' = {
+                $list = [Collections.Generic.List[Object]] @{}
+            }
+            'strong typing'           = {
+                [Collections.Generic.List[Object]] $list = @()
+            }
+        } -GroupName $Iterations
+    }
+} else {
+    Import-Module .\measure.psm1
+
+    Measure-ScriptBlock -Method 'New-Object' -Iterations $Max -ScriptBlock {
+        $list = New-Object System.Collections.Generic.List[Object]
+    }
+
+    Measure-ScriptBlock -Method 'casting empty array' -Iterations $Max -ScriptBlock {
+        $list = [Collections.Generic.List[Object]] @()
+    }
+    Measure-ScriptBlock -Method 'strong typing' -Iterations $Max -ScriptBlock {
+        [Collections.Generic.List[Object]] $list = @()
+    }
+
+    Remove-Module measure
 }
