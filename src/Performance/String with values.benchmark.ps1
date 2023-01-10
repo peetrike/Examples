005926e30341371b1f1342a0cf4469f899c3bb2c
Peter Wawa <meelisn@outlook.com>
2023-01-10 14:06:33 +0200

    replace double quotes with single ones

--- src/Performance/String with values.benchmark.ps1
+++ src/Performance/String with values.benchmark.ps1
@@ -14,24 +14,24 @@ for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
             $result = "this is 1: $value1 and 2: $value2"
         }
         'concatenation'       = {
-            $result = "this is 1: " + $value1 + " and 2: " + $value2
+            $result = 'this is 1: ' + $value1 + ' and 2: ' + $value2
         }
         'Single quote concat' = {
             $result = 'this is 1: ' + $value1 + ' and 2: ' + $value2
         }
         'StringBuilder'       = {
             $sb = [Text.StringBuilder] @{ Capacity = 26 }
-            $null = $sb.Append("this is 1: ")
+            $null = $sb.Append('this is 1: ')
             $null = $sb.Append($value1)
-            $null = $sb.Append(" and 2: ")
+            $null = $sb.Append(' and 2: ')
             $null = $sb.Append($value2)
-            $result = $sb.tostring()
+            $result = $sb.ToString()
         }
         '-f operator'         = {
             $result = 'this is 1: {0} and 2: {1}' -f $value1, $value2
         }
         '-join operator'      = {
-            $result = "this is 1:", $value1, "and 2:", $value2 -join ' '
+            $result = 'this is 1:', $value1, 'and 2:', $value2 -join ' '
         }
     } -GroupName ('{0} times' -f $iterations)
 }
