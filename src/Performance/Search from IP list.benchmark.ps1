708fdb1f1fadc6583ce0bdcbba33ae1e9cede23a
Peter Wawa <meelisn@outlook.com>
2023-01-17 18:22:15 +0200

    add IP list search benchmark

---
+++ src/Performance/Search from IP list.benchmark.ps1
@@ -0,0 +1,39 @@
+﻿#Requires -Module BenchPress
+
+param (
+    $Min = 10,
+    $Max = 1000,
+    $Repeat = 1
+)
+
+$source = '1.2.3.4;10.11.12.13'
+$IP = '1.2.3.4'
+
+for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+    Measure-Benchmark -RepeatCount $Repeat -Technique @{
+        '-match'  = {
+            $source = $source
+            $IP = $IP
+            $iterations = $iterations
+            foreach ($i in 1..$iterations) {
+                $source -match $IP
+            }
+        }
+        '-split'  = {
+            $source = $source
+            $IP = $IP
+            $iterations = $iterations
+            foreach ($i in 1..$iterations) {
+                ($source -split ';') -contains $IP
+            }
+        }
+        'Split()' = {
+            $source = $source
+            $IP = $IP
+            $iterations = $iterations
+            foreach ($i in 1..$iterations) {
+                $source.Split(';') -contains $IP
+            }
+        }
+    } -GroupName ('{0} times' -f $iterations)
+}
