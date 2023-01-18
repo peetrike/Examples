900bcf7d500440210cfb10968241e6ae76500ab6
Peter Wawa <meelisn@outlook.com>
2023-01-18 16:13:24 +0200

    Add sample function
    this function copies object using serialization

---
+++ src/Functions/Copy-Object.ps1
@@ -0,0 +1,49 @@
+﻿<#
+    .LINK
+        https://learn.microsoft.com/dotnet/standard/serialization/binaryformatter-security-guide
+#>
+[CmdletBinding()]
+param (
+        [Parameter(
+            Mandatory,
+            ValueFromPipeline
+        )]
+    $InputObject
+)
+
+begin {
+    function Copy-Object {
+        <#
+            .NOTES
+                The BinaryFormatter type is dangerous and is not recommended for data processing.
+                Applications should stop using BinaryFormatter as soon as possible,
+                even if they believe the data they're processing to be trustworthy.
+                BinaryFormatter is insecure and can't be made secure.
+            .LINK
+                https://learn.microsoft.com/dotnet/standard/serialization/binaryformatter-security-guide
+        #>
+        [CmdletBinding()]
+        param (
+                [Parameter(
+                    Mandatory,
+                    ValueFromPipeline
+                )]
+            $InputObject
+        )
+
+        begin {
+            $memStream = New-Object IO.MemoryStream
+            $formatter = New-Object Runtime.Serialization.Formatters.Binary.BinaryFormatter
+        }
+
+        process {
+            $formatter.Serialize($memStream, $InputObject)
+            $memStream.Position = 0
+            $formatter.Deserialize($memStream)
+        }
+    }
+}
+
+process {
+    $InputObject | Copy-Object
+}
