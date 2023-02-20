872189c9efc67ccb43aff1a903faa99e0958111b
Peter Wawa <meelisn@outlook.com>
2023-02-20 11:42:53 +0200

    Refactor script
    - fix -TimeZone default value
    - add function call and parameters to script

---
+++ src/Functions/timeconversion.ps1
@@ -0,0 +1,63 @@
+﻿#Requires -Version 5.0
+
+[OutputType([DateTimeOffset])]
+[CmdletBinding()]
+param(
+    [ArgumentCompleter({
+        param ($commandName, $parameterName, $wordToComplete)
+
+        foreach ($dt in [TimeZoneInfo]::GetSystemTimeZones()) {
+            if ($dt.id, $dt.displayname -match $wordToComplete) {
+                [Management.Automation.CompletionResult]::new(
+                    ("'{0}'" -f $dt.id),
+                    $dt.DisplayName,
+                    'ParameterValue',
+                    ('{0}/{1} (DST:{2})' -f @(
+                        $dt.StandardName
+                        $dt.DaylightName
+                        $dt.SupportsDaylightSavingTime
+                    ))
+                )
+            }
+        }
+    })]
+    [Alias('tz')]
+    [string]
+$TimeZone = [TimeZoneInfo]::Local.Id,
+    [DateTimeOffset]
+$Time = [datetime]::Now
+)
+function Get-DtoAt {
+    [OutputType([DateTimeOffset])]
+    param(
+            [ArgumentCompleter({
+                param ($commandName, $parameterName, $wordToComplete)
+
+                foreach ($dt in [TimeZoneInfo]::GetSystemTimeZones()) {
+                    if ($dt.id, $dt.displayname -match $wordToComplete) {
+                        [Management.Automation.CompletionResult]::new(
+                            ("'{0}'" -f $dt.id),
+                            $dt.DisplayName,
+                            'ParameterValue',
+                            ('{0}/{1} (DST:{2})' -f @(
+                                $dt.StandardName
+                                $dt.DaylightName
+                                $dt.SupportsDaylightSavingTime
+                            ))
+                        )
+                    }
+                }
+            })]
+            [Alias('tz')]
+            [string]
+        $TimeZone = [TimeZoneInfo]::Local.Id,
+            [DateTimeOffset]
+        $Time = [datetime]::Now
+    )
+
+    $time.ToOffset(
+        [System.TimeZoneInfo]::FindSystemTimeZoneById($TimeZone).GetUtcOffset($time.UtcDateTime)
+    )
+}
+
+Get-DtoAt @PSBoundParameters
