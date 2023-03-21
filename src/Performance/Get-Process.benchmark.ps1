d9c71d96d417d3b1541d67c77c907476b8808a64
Peter Wawa <meelisn@outlook.com>
2023-03-21 20:45:47 +0200

    add accelerator to PS2 tests

--- src/Performance/Get-Process.benchmark.ps1
+++ src/Performance/Get-Process.benchmark.ps1
@@ -2,6 +2,7 @@
 
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCmdLets', 'Get-CimInstance')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWMICmdlet', '')]
+[CmdletBinding()]
 param (
     $Min = 1,
     $Max = 10
@@ -10,20 +11,23 @@ param (
 if ($PSVersionTable.PSVersion.Major -gt 2) {
     $Technique = @{
         'Get-Process' = {
-            $null = (Get-Process -id $PID).Name
+            (Get-Process -id $PID).Name
         }
         '.NET direct' = {
-            $null = [Diagnostics.Process]::GetCurrentProcess().Name
+            [Diagnostics.Process]::GetProcessById($PID).Name
+        }
+        'Accelerator' = {
+            ([wmi] "Win32_Process.Handle=$PID").Name
         }
     }
 
     if (Get-Command Get-CimInstance -ErrorAction SilentlyContinue) {
         $Technique += @{
             'GCIM'      = {
-                $null = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
+                (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
             }
             'GCIM full' = {
-                $null = (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID").Name
+                (Get-CimInstance -ClassName Win32_Process -Filter "ProcessId = $PID").Name
             }
         }
     }
@@ -31,10 +35,10 @@ if ($PSVersionTable.PSVersion.Major -gt 2) {
     if ($PSVersionTable.PSVersion.Major -le 5) {
         $Technique += @{
             'GWMI'      = {
-                $null = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
+                (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
             }
             'GWMI full' = {
-                $null = (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID").Name
+                (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID").Name
             }
         }
     }
@@ -46,9 +50,13 @@ if ($PSVersionTable.PSVersion.Major -gt 2) {
     Write-Verbose -Message 'PowerShell 2'
     Import-Module .\measure.psm1
     Measure-ScriptBlock -Method '.NET' -Iterations $Max -ScriptBlock {
-        [Diagnostics.Process]::GetCurrentProcess().Name
+        [Diagnostics.Process]::GetProcessById($PID).Name
     }
     Measure-ScriptBlock -Method 'cmdlet' -Iterations $Max -ScriptBlock { (Get-Process -id $PID).Name }
+    Measure-ScriptBlock -Method 'Accelerator' -Iterations $Max -ScriptBlock {
+        ([wmi] "Win32_Process.Handle=$PID").Name
+    }
+
     Measure-ScriptBlock -Method 'WMI' -Iterations $Max -ScriptBlock {
         (Get-WmiObject -Class Win32_Process -Filter "ProcessId = $PID" -Property Name).Name
     }
