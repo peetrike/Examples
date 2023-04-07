123bbb39e9a114f1a72fc0858f2dc0586e05e05d
Peter Wawa <meelisn@outlook.com>
2023-04-07 09:06:28 +0300

    add script to create Scheduled task with ACL

---
+++ src/Snippets/create-task.ps1
@@ -0,0 +1,26 @@
+﻿# Create the task
+$action = New-ScheduledTaskAction -Execute cmd.exe -Argument '/c whoami > C:\temp\out.txt'
+$principal = New-ScheduledTaskPrincipal -UserId SYSTEM -LogonType ServiceAccount
+$task = Register-ScheduledTask -TaskName MyTask -Action $action -Principal $principal
+
+# Get the task SecurityDescriptor
+$scheduler = New-Object -ComObject Schedule.Service
+$scheduler.Connect()
+$comTask = $scheduler.GetFolder($task.TaskPath).GetTask($task.TaskName)
+$taskSDDL = $comTask.GetSecurityDescriptor(0xF)
+$taskSD = [System.Security.AccessControl.RawSecurityDescriptor]::new($taskSDDL)
+
+# Add rule for Users to read and execute the task
+$usersGroup = [System.Security.Principal.NTAccount]::new('Users')
+$readAndExecuteAce = [System.Security.AccessControl.CommonAce]::new(
+    'None',
+    'AccessAllowed',
+    0xA0000000,  # GENERIC_READ, GENERIC_EXECUTE
+    $usersGroup.Translate([System.Security.Principal.SecurityIdentifier]),
+    $false,
+    $null)
+$taskSD.DiscretionaryAcl.InsertAce($taskSD.DiscretionaryAcl.Count, $readAndExecuteAce)
+$newSDDL = $taskSD.GetSddlForm('All')
+
+# Set the new SecurityDescriptor on the task
+$comTask.SetSecurityDescriptor($newSDDL, 0)
