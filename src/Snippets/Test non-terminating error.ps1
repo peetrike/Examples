ad1c227e3e5135f5159645b355aa3a2bc738c6ab
Peter Wawa <meelisn@outlook.com>
2023-04-17 17:55:53 +0300

    add non-terminating error test script

---
+++ src/Snippets/Test non-terminating error.ps1
@@ -0,0 +1,34 @@
+﻿
+# how to stop processing current item in pipeline without breaking whole pipeline
+
+function test-asi {
+    [CmdletBinding()]
+    param (
+            [Parameter(
+                ValueFromPipeline
+            )]
+            [string]
+        $Asi,
+            [ValidateSet(
+                'Break',
+                'Continue',
+                'Return'
+            )]
+            [string]
+        $Test
+    )
+
+    process {
+        if ($asi -like 'kaks') {
+            write-error -Message 'Kaks pole lubatud'
+            switch ($test) {
+                'break' { break }
+                'continue' { continue }
+                'return' { return }
+            }
+        } else { $Asi }
+        Write-Verbose -Message 'ending processing pipe'
+    }
+}
+
+'yks', 'kaks', 'kolm' | test-asi -Test Return | foreach { @{ Väärtus = $_} }
