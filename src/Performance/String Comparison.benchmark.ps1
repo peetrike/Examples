79c5406a7be9f53a7342b801abb8f04842e45e0e
Peter Wawa <meelisn@outlook.com>
2023-04-27 10:26:42 +0300

    Add String comparison benchmark script

---
+++ src/Performance/String Comparison.benchmark.ps1
@@ -0,0 +1,31 @@
+﻿#Requires -Modules benchpress
+
+[CmdletBinding()]
+param (
+    $Min = 100,
+    $Max = 10000
+)
+
+
+$string1 = 'tere vana kere'
+$string2 = 'tere vana kere'
+
+for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+    Measure-Benchmark -RepeatCount $iterations -Technique @{
+        '-eq'            = {
+             $string1 -eq $string2
+        }
+        '-like pattern'  = {
+            $string1 -like '*vana*'
+        }
+        '-like full'     = {
+            $string1 -like $string2
+        }
+        '-match pattern' = {
+            $string1 -match 'vana'
+        }
+        '-match full'    = {
+            $string1 -match $string2
+        }
+    } -GroupName ('{0} times' -f $iterations)
+}
