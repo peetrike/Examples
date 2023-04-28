ca77dc1af10acdc418384c8514bc91197750d01c
Peter Wawa <meelisn@outlook.com>
2023-04-28 17:35:11 +0300

    Use same scriptblocks in PS2 tests

--- src/Performance/Get-Service.benchmark.ps1
+++ src/Performance/Get-Service.benchmark.ps1
@@ -12,24 +12,27 @@ param (
 )
 
 Add-Type -AssemblyName System.ServiceProcess
+$dotNet = {
+    [ServiceProcess.ServiceController] 'bits'
+}
+$Cmdlet = {
+    Get-Service 'Bits'
+}
+$Accelerator = {
+    [wmi] "Win32_Service.Name='Bits'"
+}
+$WmiFull = {
+    Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'"
+}
+$WmiSpecific = {
+    Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
+}
 
 if ($PSVersionTable.PSVersion.Major -gt 2) {
     $Technique = @{
-        <# 'Get-Service | Where' = {
-            Get-Service | Where-Object { $_.Name -like "Bits" }
-        } #>
-        'Get-Service' = {
-            Get-Service 'Bits'
-        }
-        <# '.NET | Where'        = {
-            [ServiceProcess.ServiceController]::GetServices() | Where-Object { $_.Name -like 'bits' }
-        } #>
-        '.NET direct' = {
-            [ServiceProcess.ServiceController] 'bits'
-        }
-        'Accelerator' = {
-            [wmi] "Win32_Service.Name='Bits'"
-        }
+        'cmdlet'      = $Cmdlet
+        '.NET'        = $dotNet
+        'Accelerator' = $Accelerator
         'GCIM'        = {
             Get-CimInstance -ClassName Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
         }
@@ -40,12 +43,8 @@ if ($PSVersionTable.PSVersion.Major -gt 2) {
 
     if ($PSVersionTable.PSVersion.Major -le 5) {
         $Technique += @{
-            'GWMI'      = {
-                Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
-            }
-            'GWMI full' = {
-                Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'"
-            }
+            'GWMI'      = $WmiSpecific
+            'GWMI full' = $WmiFull
         }
     }
 
@@ -56,17 +55,9 @@ if ($PSVersionTable.PSVersion.Major -gt 2) {
     Write-Verbose -Message 'PowerShell 2'
     Import-Module .\measure.psm1
 
-    Measure-ScriptBlock -Method '.NET' -Iterations $Max -ScriptBlock {
-        [ServiceProcess.ServiceController] 'bits'
-    }
-    Measure-ScriptBlock -Method 'cmdlet' -Iterations $Max -ScriptBlock {
-        Get-Service 'Bits'
-    }
+    Measure-ScriptBlock -Method '.NET' -Iterations $Max -ScriptBlock $dotNet
+    Measure-ScriptBlock -Method 'cmdlet' -Iterations $Max -ScriptBlock $Cmdlet
 
-    Measure-ScriptBlock -Method 'Accelerator' -Iterations $Max -ScriptBlock {
-        [wmi] "Win32_Service.Name='Bits'"
-    }
-    Measure-ScriptBlock -Method 'GWMI' -Iterations $Max -ScriptBlock {
-        Get-WmiObject -Class Win32_Service -Filter "Name = 'BITS'" -Property Name, PathName
-    }
+    Measure-ScriptBlock -Method 'Accelerator' -Iterations $Max -ScriptBlock $Accelerator
+    Measure-ScriptBlock -Method 'GWMI' -Iterations $Max -ScriptBlock $WmiSpecific
 }
