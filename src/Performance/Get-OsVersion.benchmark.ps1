ffed0e338ded7cf5042b5f9dc1eafb33deeb6b19
Peter Wawa <meelisn@outlook.com>
2023-04-28 17:09:12 +0300

    Make test PS7 compatible

--- src/Performance/Get-OsVersion.benchmark.ps1
+++ src/Performance/Get-OsVersion.benchmark.ps1
@@ -1,5 +1,6 @@
 ﻿#Requires -Module BenchPress
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCommands', 'Get-WmiObject')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCmdLets', 'Get-CimInstance')]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWMICmdlet', '')]
 param (
@@ -7,22 +8,33 @@ param (
     $Max = 100
 )
 
-for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $iterations -Technique @{
+
+$Technique = @{
+    'GCIM only version' = {
+        [Version] (Get-CimInstance -ClassName Win32_OperatingSystem -Property Version).Version
+    }
+    'GCIM full'         = {
+        [Version] (Get-CimInstance -ClassName Win32_OperatingSystem).Version
+    }
+    'Accelerator'       = {
+        [Version] ([wmi] 'Win32_OperatingSystem=@').Version
+    }
+    '.NET'              = {
+        [System.Environment]::OSVersion.Version
+    }
+}
+
+if ($PSVersionTable.PSVersion.Major -le 5) {
+    $Technique += @{
         'GWMI full'         = {
             [Version] (Get-WmiObject -ClassName Win32_OperatingSystem).Version
         }
         'GWMI only version' = {
             [Version] (Get-WmiObject -Class Win32_OperatingSystem -Property Version).Version
         }
-        'GCIM only version' = {
-            [Version] (Get-CimInstance -ClassName Win32_OperatingSystem -Property Version).Version
-        }
-        'GCIM full'         = {
-            [Version] (Get-CimInstance -ClassName Win32_OperatingSystem).Version
-        }
-        '.NET'              = {
-            [System.Environment]::OSVersion.Version
-        }
-    } -GroupName ('{0} times' -f $iterations)
+    }
+}
+
+for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+    Measure-Benchmark -RepeatCount $iterations -Technique $Technique -GroupName ('{0} times' -f $iterations)
 }
