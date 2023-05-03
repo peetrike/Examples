a973eecf63f0f2e7fce4e6ae7e5c66427f55b24a
Peter Wawa <meelisn@outlook.com>
2023-05-03 15:23:12 +0300

    Add PowerShell 2 tests

--- src/Performance/Get-ComputerSid.Benchmark.ps1
+++ src/Performance/Get-ComputerSid.Benchmark.ps1
@@ -1,6 +1,12 @@
-﻿#Requires -Version 3
-#Requires -Module BenchPress
+﻿#Requires -Version 2
+# Requires -Module BenchPress
 
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCmdlets', 'Get-LocalUser')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCommands', 'Get-LocalUser')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCommands', 'Get-WmiObject')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseCompatibleCmdlets', 'Get-CimInstance')]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWMICmdlet', '')]
+[CmdletBinding()]
 param (
     $Min = 1,
     $Max = 100
@@ -14,28 +20,38 @@ $CimQuery = 'select Sid from {0} where {1}' -f $ClassName, $FilterSid
 
 Add-Type -AssemblyName System.DirectoryServices.AccountManagement
 
-for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $iterations -Technique @{
-        '.Net'          = {
-            $localMachine = [DirectoryServices.AccountManagement.ContextType]::Machine
-            $PrincipalContext = [DirectoryServices.AccountManagement.PrincipalContext] $localMachine
-            $UserPrincipal = [DirectoryServices.AccountManagement.UserPrincipal] $PrincipalContext
-            $searcher = [DirectoryServices.AccountManagement.PrincipalSearcher] $UserPrincipal
-            $searcher.FindOne().Sid.AccountDomainSid
-        }
-        'ADSI'          = {
-            $SidArray = ([adsi]"WinNT://$env:COMPUTERNAME/Administrator").ObjectSid.Value
-            ([Security.Principal.SecurityIdentifier]::new($SidArray, 0)).AccountDomainSid
-        }
-        'LocalUser'     = {
-            (Get-LocalUser | select -First 1).Sid.AccountDomainSid
-        }
+$dotNet = {
+    $localMachine = [DirectoryServices.AccountManagement.ContextType]::Machine
+    $PrincipalContext = [DirectoryServices.AccountManagement.PrincipalContext] $localMachine
+    $UserPrincipal = [DirectoryServices.AccountManagement.UserPrincipal] $PrincipalContext
+    $searcher = [DirectoryServices.AccountManagement.PrincipalSearcher] $UserPrincipal
+    $searcher.FindOne().Sid.AccountDomainSid
+}
+$Adsi = {
+    $SidArray = ([adsi]"WinNT://$env:COMPUTERNAME/Administrator").ObjectSid.Value
+    (
+        New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList $SidArray, 0
+    ).AccountDomainSid
+}
+$Accelerator = {
+    $Searcher = [wmisearcher] $CimQuery
+    ([Security.Principal.SecurityIdentifier]$Searcher.Get().Sid).AccountDomainSid
+}
+$WmiSid = {
+    $Object = Get-WmiObject -Class $ClassName -Filter $FilterSid
+    ([Security.Principal.SecurityIdentifier]$Object.Sid).AccountDomainSid
+}
+$Wmi = {
+    ($Object = Get-WmiObject -Class $ClassName -Filter $Filter)[0]
+    ([Security.Principal.SecurityIdentifier]$Object.Sid).AccountDomainSid
+}
+
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    $Technique = @{
+        '.Net'          = $dotNet
+        'ADSI'          = $Adsi
         'Cim'           = {
             $Object = (Get-CimInstance -ClassName $ClassName -Filter $Filter)[0]
-            ([Security.Principal.SecurityIdentifier]$Object.Sid).AccountDomainSid
-        }
-        'cim w/ select' = {
-            $Object = Get-CimInstance -ClassName $ClassName -Filter $Filter | Select-Object -First 1
             ([Security.Principal.SecurityIdentifier]$Object.Sid).AccountDomainSid
         }
         'Cim w/ Sid'    = {
@@ -47,12 +63,38 @@ for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
             ([Security.Principal.SecurityIdentifier]$Object.Sid).AccountDomainSid
         }
         'Cim w/ Props'  = {
-            $Object = Get-CimInstance -query $CimQuery #| Select-Object -First 1
+            $Object = Get-CimInstance -query $CimQuery
             ([Security.Principal.SecurityIdentifier]$Object.Sid).AccountDomainSid
         }
-        'Accelerator'   = {
-            $Searcher = [wmisearcher] $CimQuery
-            ([Security.Principal.SecurityIdentifier]$Searcher.Get().Sid).AccountDomainSid
+        'Accelerator'   = $Accelerator
+    }
+
+    if (Get-Command Get-LocalUser -ErrorAction SilentlyContinue) {
+        $Technique += @{
+            'LocalUser' = {
+                (Get-LocalUser | Select-Object -First 1).Sid.AccountDomainSid
+            }
         }
-    } -GroupName ('Time only: {0} times' -f $iterations)
+    }
+    if ($PSVersionTable.PSVersion.Major -lt 6) {
+        $Technique += @{
+            'WMI'        = $Wmi
+            'WMI w/ Sid' = $WmiSid
+        }
+    }
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        $GroupName = 'Time only: {0} times' -f $iterations
+        Measure-Benchmark -RepeatCount $iterations -Technique $Technique -GroupName $GroupName
+    }
+} else {
+    Write-Verbose -Message 'PowerShell 2'
+    Import-Module .\measure.psm1
+
+   @(
+        Measure-ScriptBlock -Method '.NET' -Iterations $Max -ScriptBlock $DotNet
+        Measure-ScriptBlock -Method 'ADSI' -Iterations $Max -ScriptBlock $Adsi
+        Measure-ScriptBlock -Method 'Accelerator' -Iterations $Max -ScriptBlock $Accelerator
+        Measure-ScriptBlock -Method 'WMI' -Iterations $Max -ScriptBlock $Wmi
+        Measure-ScriptBlock -Method 'WMI w/ Sid' -Iterations $Max -ScriptBlock $WmiSid
+    ) | Sort-Object TotalMilliseconds
 }
