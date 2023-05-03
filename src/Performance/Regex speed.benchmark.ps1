12116b6078849c677b41507941f8925bf7b2e622
Peter Wawa <meelisn@outlook.com>
2023-05-03 18:45:39 +0300

    Add PowerShell 2 tests

--- src/Performance/Regex speed.benchmark.ps1
+++ src/Performance/Regex speed.benchmark.ps1
@@ -1,4 +1,5 @@
-﻿#Requires -Modules BenchPress
+﻿#Requires -Version 2
+# Requires -Modules BenchPress
 
 param (
     $Min = 10,
@@ -10,40 +11,82 @@ $paths = @(
     '"C:\Program Files\My Zabbix Agent\zabbix_agentd.exe" --multiple-agents --config "C:\Program Files\My Zabbix Agent\zabbix_agentd.conf"'
 )
 
-for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $iterations -Technique @{
-        'regex'           = {
-            foreach ($path in $paths) {
-                $BinaryPathName, $AgentPath, $ConfigPath = (
-                    [regex] '"(.+\.exe)".+--config\s+"(.+\.conf)"'
-                ).Matches($Path).Groups.Value
-            }
+$PatternGeneric = '"(.+)".*--config "(.+)"'
+$PatternSpecific = '"(.+\.exe)".+--config\s+"(.+\.conf)"'
+
+$Operator = {
+    foreach ($path in $paths) {
+        if ($path -match $PatternGeneric) {
+            $AgentPath, $ConfigPath = $Matches[1..2]
         }
-        'assign to $null' = {
-            foreach ($path in $paths) {
-                $null, $AgentPath, $ConfigPath = (
-                    [regex] '"(.+\.exe)".+--config\s+"(.+\.conf)"'
-                ).Matches($Path).Groups.Value
-            }
-        }
-        'less specific'   = {
-            foreach ($path in $paths) {
-                $null, $AgentPath, $ConfigPath = (
-                    [regex] '"(.+)".*--config "(.+)"'
-                ).Matches($Path).Groups.Value
-            }
-        }
-        'Select-String'   = {
-            $paths | Select-String -Pattern '"(.+)".*--config "(.+)"' | ForEach-Object {
-                $null, $AgentPath, $ConfigPath = $_.Matches.Groups.Value
-            }
-        }
-        '-match'          = {
-            foreach ($path in $paths) {
-                if ($path -match '"(.+)".*--config "(.+)"') {
-                    $AgentPath, $ConfigPath = $Matches[1..2]
+    }
+}
+
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        Measure-Benchmark -RepeatCount $iterations -Technique @{
+            'Regex specific'  = {
+                foreach ($path in $paths) {
+                    $BinaryPathName, $AgentPath, $ConfigPath = (
+                        [regex] $PatternSpecific
+                    ).Matches($Path).Groups.Value
                 }
             }
+            'assign to $null' = {
+                foreach ($path in $paths) {
+                    $null, $AgentPath, $ConfigPath = (
+                        [regex] $PatternSpecific
+                    ).Matches($Path).Groups.Value
+                }
+            }
+            'Skip first'      = {
+                foreach ($path in $paths) {
+                    $AgentPath, $ConfigPath = ([regex] $PatternSpecific).Matches($Path).Groups[1..2].Value
+                }
+            }
+            'Regex generic'   = {
+                foreach ($path in $paths) {
+                    $AgentPath, $ConfigPath = (
+                        [regex] $PatternGeneric
+                    ).Matches($Path).Groups[1..2].Value
+                }
+            }
+            'Select-String'   = {
+                $paths | Select-String -Pattern $PatternGeneric | ForEach-Object {
+                    $AgentPath, $ConfigPath = $_.Matches.Groups[1..2].Value
+                }
+            }
+            '-match'          = $Operator
+        } -GroupName ('{0} times' -f $iterations)
+    }
+} else {
+    Write-Verbose -Message 'PowerShell 2'
+    Import-Module .\measure.psm1
+
+    @(
+        Measure-ScriptBlock -Method '-match' -Iterations $Max -ScriptBlock $Operator
+        Measure-ScriptBlock -Method 'Select-String' -Iterations $Max -ScriptBlock {
+            $paths | Select-String -Pattern $PatternGeneric | ForEach-Object {
+                $AgentPath, $ConfigPath = $_.Matches |
+                    Select-Object -ExpandProperty Groups |
+                    Select-Object -ExpandProperty Value -Skip 1
+            }
         }
-    } -GroupName ('{0} times' -f $iterations)
+        Measure-ScriptBlock -Method 'Regex w/ select' -Iterations $Max -ScriptBlock {
+            foreach ($path in $paths) {
+                $AgentPath, $ConfigPath = ([regex] $PatternGeneric).Matches($Path) |
+                    Select-Object -ExpandProperty Groups |
+                    Select-Object -ExpandProperty Value -Skip 1
+            }
+        }
+        Measure-ScriptBlock -Method 'Regex w/ foreach' -Iterations $Max -ScriptBlock {
+            foreach ($path in $paths) {
+                $AgentPath, $ConfigPath = (
+                    ([regex] $PatternGeneric).Matches($Path) |
+                        ForEach-Object { $_.Groups }
+                )[1..2] | ForEach-Object { $_.Value }
+            }
+        }
+
+    ) | Sort-Object -Property TotalMilliseconds
 }
