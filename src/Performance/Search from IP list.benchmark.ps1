1b6134634930b304d689597e239cb02b6f190a4b
Peter Wawa <meelisn@outlook.com>
2023-05-03 18:54:31 +0300

    Add PowerShell 2 tests

--- src/Performance/Search from IP list.benchmark.ps1
+++ src/Performance/Search from IP list.benchmark.ps1
@@ -1,4 +1,5 @@
-﻿#Requires -Modules BenchPress
+﻿#Requires -Version 2
+# Requires -Modules BenchPress
 
 param (
     $Min = 10,
@@ -9,31 +10,46 @@ param (
 $source = '1.2.3.4;10.11.12.13'
 $IP = '1.2.3.4'
 
-for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-    Measure-Benchmark -RepeatCount $Repeat -Technique @{
-        '-match'  = {
-            $source = $source
-            $IP = $IP
-            $iterations = $iterations
-            foreach ($i in 1..$iterations) {
-                $source -match $IP
-            }
-        }
-        '-split'  = {
-            $source = $source
-            $IP = $IP
-            $iterations = $iterations
-            foreach ($i in 1..$iterations) {
-                ($source -split ';') -contains $IP
-            }
-        }
-        'Split()' = {
-            $source = $source
-            $IP = $IP
-            $iterations = $iterations
-            foreach ($i in 1..$iterations) {
-                $source.Split(';') -contains $IP
-            }
-        }
-    } -GroupName ('{0} times' -f $iterations)
+$Regex = {
+    $source = $source
+    $IP = $IP
+    $iterations = $iterations
+    foreach ($i in 1..$iterations) {
+        $source -match $IP
+    }
+}
+$Operator = {
+    $source = $source
+    $IP = $IP
+    $iterations = $iterations
+    foreach ($i in 1..$iterations) {
+        ($source -split ';') -contains $IP
+    }
+}
+$Method = {
+    $source = $source
+    $IP = $IP
+    $iterations = $iterations
+    foreach ($i in 1..$iterations) {
+        $source.Split(';') -contains $IP
+    }
+}
+
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        Measure-Benchmark -RepeatCount $Repeat -Technique @{
+            '-match'  = $Regex
+            '-split'  = $Operator
+            'Split()' = $Method
+        } -GroupName ('{0} times' -f $iterations)
+    }
+} else {
+    Write-Verbose -Message 'PowerShell 2'
+    Import-Module .\measure.psm1
+
+    @(
+        Measure-ScriptBlock -Method '-match' -Iterations $Max -ScriptBlock $Regex
+        Measure-ScriptBlock -Method '-split' -Iterations $Max -ScriptBlock $Operator
+        Measure-ScriptBlock -Method 'Split()' -Iterations $Max -ScriptBlock $Method
+    ) | Sort-Object -Property TotalMilliseconds
 }
