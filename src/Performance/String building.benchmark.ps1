394100bada5a4721742beaf5871fcb599b0437b6
Peter Wawa <meelisn@outlook.com>
2023-06-14 22:45:04 +0300

    PS 2.0: use same technique hashtable for tests

--- src/Performance/String building.benchmark.ps1
+++ src/Performance/String building.benchmark.ps1
@@ -1,5 +1,6 @@
 ﻿# Requires -Module BenchPress
 
+[CmdletBinding()]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', 'string')]
 param (
     $Min = 10,
@@ -7,40 +8,37 @@ param (
     $Repeat = 1
 )
 
-$Adding = {
-    $string = ''
-    $Iterations = $Iterations
-    1..$Iterations | ForEach-Object {
-        $string += 'tere'
+$Technique = @{
+    'string +='     = {
+        $string = ''
+        $Iterations = $Iterations
+        1..$Iterations | ForEach-Object {
+            $string += 'tere'
+        }
     }
-}
-$Join = {
-    $Iterations = $Iterations
-    $string = @(
-        1..$Iterations | ForEach-Object { 'tere' }
-    ) -join ''
-}
-$Builder = {
-    $sb = [Text.StringBuilder] 4
-    $Iterations = $Iterations
-    1..$Iterations | ForEach-Object { [void] $sb.Append('tere') }
-    $string = $sb.ToString()
+    '-join'         = {
+        $Iterations = $Iterations
+        $string = @(
+            1..$Iterations | ForEach-Object { 'tere' }
+        ) -join ''
+    }
+    'StringBuilder' = {
+        $sb = [Text.StringBuilder] 4
+        $Iterations = $Iterations
+        1..$Iterations | ForEach-Object { [void] $sb.Append('tere') }
+        $string = $sb.ToString()
+    }
 }
 
 if ($PSVersionTable.PSVersion.Major -gt 2) {
     for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
-        Measure-Benchmark -RepeatCount $Repeat -Technique @{
-            'string +='     = $Adding
-            '-join'         = $Join
-            'StringBuilder' = $Builder
-        } -GroupName ('{0} times' -f $iterations)
+        Measure-Benchmark -RepeatCount $Repeat -Technique $Technique -GroupName ('{0} times' -f $iterations)
     }
 } else {
+    Write-Verbose -Message ('PowerShell 2: {0} times' -f $Max)
     Import-Module .\measure.psm1
 
-    @(
-        Measure-ScriptBlock -Method 'string +=' -Iterations $Repeat -ScriptBlock $Adding
-        Measure-ScriptBlock -Method '-join' -Iterations $Repeat -ScriptBlock $Join
-        Measure-ScriptBlock -Method 'StringBuilder' -Iterations $Repeat -ScriptBlock $Builder
-    ) | Sort-Object -Property TotalMilliseconds
+    foreach ($key in $Technique.Keys) {
+        Measure-ScriptBlock -Method $key -Iterations $max -ScriptBlock $Technique.$key
+    }
 }
