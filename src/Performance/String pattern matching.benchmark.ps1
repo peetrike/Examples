a772d702e7e380e076dbd395ce5e222ae8f8753d
Peter Wawa <meelisn@outlook.com>
2023-06-14 22:27:20 +0300

    add pattern matching test

---
+++ src/Performance/String pattern matching.benchmark.ps1
@@ -0,0 +1,45 @@
+﻿# Requires -Modules benchpress
+
+[CmdletBinding()]
+param (
+    $Min = 100,
+    $Max = 10000
+)
+
+$string = 'tere vana kere'
+$pattern = '*vana*'
+$rePattern = 'vana'
+
+$Wildcard = [Management.Automation.WildcardPattern] $pattern
+$regex = [Text.RegularExpressions.Regex] $rePattern
+
+$technique = @{
+    '-like pattern'  = {
+        $string -like $pattern
+    }
+    '-match pattern' = {
+        $string -match $rePattern
+    }
+    'WildCard'       = {
+        $Wildcard.IsMatch($string)
+    }
+    'Regex'          = {
+        $regex.Match($string).Success
+    }
+    'Static regex'   = {
+        [regex]::IsMatch($string, $rePattern)
+    }
+}
+
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        Measure-Benchmark -RepeatCount $iterations -Technique $technique -GroupName ('{0} times' -f $iterations)
+    }
+} else {
+    Write-Verbose -Message 'PowerShell 2'
+    Import-Module .\measure.psm1
+
+    foreach ($key in $technique.Keys) {
+        Measure-ScriptBlock -Method $key -Iterations $Max -ScriptBlock $technique.$key
+    }
+}
