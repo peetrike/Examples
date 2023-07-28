8bd11601c771b439f27afb1e732926964fb316bb
Peter Wawa <meelisn@outlook.com>
2023-07-28 16:23:29 +0300

    Update comment based help

--- src/Gui/Read-Choice.ps1
+++ src/Gui/Read-Choice.ps1
@@ -1,23 +1,31 @@
 ﻿function Read-Choice {
     <#
-    .SYNOPSIS
-        Uses PromptForChoice host function to ask for choices
-    .DESCRIPTION
-        This helper function uses interactive choice menu to let user choose the value
-    .NOTES
-        Choices hashtable will become the choices to choose between.
-        Each element's key is used as Label and value is used as HelpMessage.
-        Label can contain character '&' to identify the next character in the label as a "hot key".
-    .LINK
-        https://docs.microsoft.com/dotnet/api/system.management.automation.host.pshostuserinterface.promptforchoice
-    .EXAMPLE
-        $Choices = @{
-            '&one' = 'choice one'
-            't&wo' = 'second choice'
-        }
-        Read-Choice -Message 'please make your choice' -Choice $choices -default 'two'
+        .SYNOPSIS
+            Uses PromptForChoice host function to ask for choices
+        .DESCRIPTION
+            This helper function uses interactive choice menu to let user choose the value
+        .NOTES
+            Choices dictionary will become the choices to choose between.
+            Each element's Key is used as Label and Value is used as HelpMessage.
+            Label can contain character '&' to identify the next character in the label as a "hot key".
+        .LINK
+            https://docs.microsoft.com/dotnet/api/system.management.automation.host.pshostuserinterface.promptforchoice
+        .EXAMPLE
+            $Choice = @{
+                '&one' = 'choice one'
+                't&wo' = 'second choice'
+            }
+            Read-Choice -Message 'please make your choice' -Choices $Choice -default 'two'
 
-        This example lets user to choose between 2 choices.
+            This example lets user to choose between 2 choices.
+        .EXAMPLE
+            $Choice = [ordered] @{
+                '&one' = 'choice one'
+                't&wo' = 'second choice'
+            }
+            Read-Choice -Message 'please make your choice' -Choices $Choice -default 'two'
+
+            This example uses OrderedDictionary to maintain choices order.
     #>
 
     [OutputType([string])]
@@ -32,7 +40,7 @@
         $Message = 'Make a choice',
             [Parameter(Mandatory)]
             [Collections.IDictionary]
-            # Choices in the form of hashtable.
+            # Choices in the form of hashtable or OrderedDictionary.
         $Choices,
             [string]
             # The label of the default value (without hotkey)
