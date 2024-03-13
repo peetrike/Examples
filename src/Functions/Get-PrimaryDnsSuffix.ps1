d2d99195f4f9cc2c08906af5aa57b0a34adb705d
Peter Wawa <meelisn@outlook.com>
2024-03-13 15:26:50 +0200

    Add DNS Primary suffix function

---
+++ src/Functions/Get-PrimaryDnsSuffix.ps1
@@ -0,0 +1,11 @@
+﻿function Get-PrimaryDnsSuffix {
+    <#
+        .LINK
+            https://learn.microsoft.com/windows/win32/sysinfo/computer-names
+    #>
+    [OutputType([string])]
+	param ()
+
+	# https://learn.microsoft.com/dotnet/api/system.net.networkinformation.ipglobalproperties
+    [Net.NetworkInformation.IPGlobalProperties]::GetIPGlobalProperties().DomainName
+}
