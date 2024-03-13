d1778766685569bea89acd87ee5bb2f203b665f3
Peter Wawa <meelisn@outlook.com>
2024-03-13 15:32:27 +0200

    Add logon session function

---
+++ src/Functions/Get-LogonSession.ps1
@@ -0,0 +1,47 @@
+﻿function Get-LogonSession {
+    [CmdletBinding()]
+    param (
+            [switch]
+        $IncludeProcess
+    )
+
+    enum SessionType {
+        System
+        Interactive = 2
+        Network
+        Batch
+        Service
+        Proxy
+        Unlock
+        NetworkClearText
+        NewCredentials
+        RemoteInteractive
+        CachedInteractive
+        CachedRemoteInteractive
+        CachedUnlock
+    }
+    $PropertySet = @(
+        'AuthenticationPackage'
+        'StartTime'
+        'LogonId'
+    )
+
+    foreach ($Session in Get-CimInstance -ClassName Win32_LogonSession) {
+        $SessionProps = @{
+            LogonType = [SessionType] $Session.LogonType
+        }
+        foreach ($property in $PropertySet) {
+            $SessionProps.$property = $Session.$property
+        }
+        $User = Get-CimAssociatedInstance -InputObject $Session -Association Win32_LoggedonUser
+
+        $SessionProps.User = $User
+        if ($IncludeProcess) {
+            $SessionProps.Process =
+                Get-CimAssociatedInstance -InputObject $Session -Association Win32_SessionProcess
+        }
+        [PSCustomObject] $SessionProps
+    }
+}
+
+Get-LogonSession
