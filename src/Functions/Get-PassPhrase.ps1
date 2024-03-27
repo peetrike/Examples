e4910e19e60624c56965a93c1241e02398bf02fd
Peter Wawa <meelisn@outlook.com>
2024-03-27 08:30:31 +0200

    add passphrase generation samples

---
+++ src/Functions/Get-PassPhrase.ps1
@@ -0,0 +1,54 @@
+﻿<#
+    .LINK
+        https://www.getpassphrase.app/blogs/api
+#>
+
+[CmdletBinding()]
+param (
+        [ValidateRange(1, 10)]
+        [int]
+    $Words,
+        [ValidateSet('-', '_', '|', '*', '.')]
+        [char]
+    $Symbol,
+        [switch]
+    $Number,
+        [switch]
+    $Capital
+)
+
+function Get-PassPhrase {
+    [CmdletBinding()]
+    param (
+            [ValidateRange(1, 10)]
+            [int]
+        $Words,
+            [ValidateSet('-', '_', '|', '*', '.')]
+            [char]
+        $Symbol,
+            [switch]
+        $Number,
+            [switch]
+        $Capital
+    )
+
+    $BaseUrl = 'https://api.getpassphrase.app/generate'
+    $Properties = @{}
+    if ($Words) {
+        $Properties['totalWords'] = $Words
+    }
+    if ($Symbol) {
+        $Properties['symbol'] = $Symbol
+    }
+    if ($Number) {
+        $Properties['includeNumber'] = 'true'
+    }
+    if ($Capital) {
+        $Properties['isCapital'] = 'true'
+    }
+    Write-Debug -Message ($Properties | ConvertTo-Json)
+    $Result = Invoke-RestMethod -uri $BaseUrl -Body $Properties
+    $Result.passphrase
+}
+
+Get-PassPhrase @PSBoundParameters
---
+++ src/Functions/Get-PassPhraseET.ps1
@@ -0,0 +1,59 @@
+﻿<#
+    .LINK
+        https://www.täringvara.ee/
+#>
+
+[CmdletBinding()]
+param (
+        [ValidateRange(1, 10)]
+        [int]
+    $Words,
+        [ValidateSet('-', '_', '|', '*', '.', ' ')]
+        [char]
+    $Symbol,
+        [switch]
+    $Number,
+        [switch]
+    $Capital
+)
+
+function Get-PassPhraseET {
+    [CmdletBinding()]
+    param (
+            [ValidateRange(1, 10)]
+            [int]
+        $Words = 4,
+            [ValidateSet('-', '_', '|', '*', '.', ' ')]
+            [char]
+        $Symbol = ' ',
+            [switch]
+        $Number,
+            [switch]
+        $Capital
+    )
+
+    $localFile = "$env:TEMP\Taringvara.txt"
+    #$BaseUrl = 'https://raw.githubusercontent.com/KaarelP2rtel/taringvara/master/docs/files/Taringvara.txt'
+    #$BaseUrl = 'http://www.täringvara.ee/files/Taringvara.txt'
+    $BaseUrl = 'http://www.xn--tringvara-v2a.ee/files/Taringvara.txt'
+    if (-not (Test-Path $localfile)) {
+        Invoke-WebRequest -Uri $BaseUrl -OutFile $localfile
+    }
+    $WordList = Get-Content -Path $localFile
+    $Selection = $WordList | Where-Object { $_ } | Get-Random -Count $Words
+    if ($Number) {
+        $i = Get-Random -Maximum ($Words)
+        $Selection[$i] += Get-Random -Maximum 10
+    }
+
+    if ($Capital) {
+        $i = Get-Random -Maximum ($Words)
+        $word = $Selection[$i]
+        $char = $word[0]
+        $Selection[$i] = ([string]$char).ToUpper() + $word.trimstart($char)
+    }
+
+    $Selection -join $Symbol
+}
+
+Get-PassPhraseET @PSBoundParameters
