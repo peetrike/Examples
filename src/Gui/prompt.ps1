7aec0eceb03e72ebfda0656f2697edb0b077b189
Peter Wawa <meelisn@outlook.com>
2024-03-27 08:30:10 +0200

    add prompt dialog sample

---
+++ src/Gui/prompt.ps1
@@ -0,0 +1,12 @@
+﻿$Title = 'add missing information'
+$Message = 'Enter the field values'
+
+$field1 = [Management.Automation.Host.fieldDescription] 'field1'
+$field1.Label = 'Field &1'
+$field1.HelpMessage = 'Help message'
+
+$field2 = [Management.Automation.Host.fieldDescription] 'field2'
+$field2.Label = 'Field &2'
+$field2.DefaultValue = 'default value'
+
+$Host.UI.Prompt($Title, $Message, @($field1, $field2))
