8593448f5767411be84afc8f8b65291261039367
Peter Wawa <meelisn@outlook.com>
2024-05-06 12:11:33 +0300

    Add LockScreen example

---
+++ src/Snippets/Lock-Screen.ps1
@@ -0,0 +1,10 @@
+﻿Add-Type -TypeDefinition @'
+    using System;
+    using System.Runtime.InteropServices;
+    public static class NativeMethods {
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+    }
+'@
+
+[NativeMethods]::LockWorkStation()
