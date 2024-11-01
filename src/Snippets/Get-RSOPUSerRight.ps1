5121fbc3c463bf30f0532ef6ffd8c3404a62edd0
Peter Wawa <meelisn@outlook.com>
2024-11-01 16:26:30 +0200

    add new sample script

---
+++ src/Snippets/Get-RSOPUSerRight.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+param (
+        [SupportsWildcards()]
+        [string]
+    $RightName = 'SeDenyInteractiveLogonRight',
+        [switch]
+    $AccountOnly
+)
+
+$searcher = [wmisearcher] @{
+    Query = "select * from RSOP_UserPrivilegeRight where UserRight LIKE '$RightName'"
+    Scope = 'ROOT\rsop\computer'
+}
+
+$result = $searcher.Get()
+
+if ($AccountOnly) {
+    $result.AccountList
+} else {
+    $result
+}
