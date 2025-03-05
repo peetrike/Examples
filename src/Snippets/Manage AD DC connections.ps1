006914f3dd77fd6df567709d441158b957c466a8
Peter Wawa <meelisn@outlook.com>
2025-03-05 17:30:19 +0200

    add new snippet

---
+++ src/Snippets/Manage AD DC connections.ps1
@@ -0,0 +1,13 @@
+﻿
+# get current DC for computer:
+[DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().FindDomainController()
+
+# get current DC for user:
+[DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().FindDomainController()
+
+# Get Global Catalog Server:
+[DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().FindGlobalCatalog()
+
+# force rediscovery of DC:
+$Locator = [DirectoryServices.ActiveDirectory.LocatorOptions]::ForceRediscovery
+[DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().FindDomainController($Locator)
