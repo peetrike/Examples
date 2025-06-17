04ab9f8c0f2628850f4b387f0669b73bd9fa24b5
Peter Wawa <meelisn@outlook.com>
2025-06-17 09:20:40 +0300

    add enums to tests

--- src/Performance/Test-IsDC.benchmark.ps1
+++ src/Performance/Test-IsDC.benchmark.ps1
@@ -6,20 +6,55 @@ param (
     $Max = 100
 )
 
+try {
+        $null = [Microsoft.PowerShell.Commands.DomainRole]::BackupDomainController
+} catch {
+    Add-Type -TypeDefinition @"
+        using System;
+        namespace Microsoft.PowerShell.Commands {
+            public enum DomainRole {
+                StandaloneWorkstation,
+                MemberWorkstation,
+                StandaloneServer,
+                MemberServer,
+                BackupDomainController,
+                PrimaryDomainController
+            }
+            public enum ProductType {
+                Unknown,
+                Workstation,
+                DomainController,
+                Server
+            }
+        }
+"@
+}
+
 $Technique = @{
     'CS Specific' = {
         $Property = 'DomainRole'
-        ([wmisearcher] "SELECT $Property FROM Win32_ComputerSystem").Get().$Property
+        $Role = ([wmisearcher] "SELECT $Property FROM Win32_ComputerSystem").Get() |
+            Select-Object -ExpandProperty $Property
+        [Microsoft.PowerShell.Commands.DomainRole] $Role
     }
     'CS Generic'  = {
-        ([wmisearcher] 'SELECT * FROM Win32_ComputerSystem').Get().DomainRole
+        $Role = ([wmisearcher] 'SELECT * FROM Win32_ComputerSystem').Get() |
+            Select-Object -ExpandProperty DomainRole
+        [Microsoft.PowerShell.Commands.DomainRole] $Role
     }
     'OS Specific' = {
         $Property = 'ProductType'
-        ([wmisearcher] "SELECT $Property FROM Win32_OperatingSystem").Get().$Property
+        $Role = ([wmisearcher] "SELECT $Property FROM Win32_OperatingSystem").Get() |
+            Select-Object -ExpandProperty $Property
+        [Microsoft.PowerShell.Commands.ProductType] $Role
     }
     'OS Generic'  = {
-        ([wmisearcher] 'SELECT * FROM Win32_OperatingSystem').Get().ProductType
+        $Role = ([wmisearcher] 'SELECT * FROM Win32_OperatingSystem').Get() |
+            Select-Object -ExpandProperty ProductType
+        [Microsoft.PowerShell.Commands.ProductType] $Role
+    }
+    'WMI'         = {
+        [Microsoft.PowerShell.Commands.ProductType] ([wmi] "Win32_ComputerSystem='$env:COMPUTERNAME'").DomainRole
     }
 }
 
