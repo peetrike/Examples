ae39e71a4ca4cc803eda8c033754a8e16326c58c
Peter Wawa <meelisn@outlook.com>
2025-06-17 09:22:32 +0300

    Add PowerShell 7 remoting setup fix

---
+++ src/Scripts/Update-PS7Plugin.ps1
@@ -0,0 +1,17 @@
+﻿Get-PSSessionConfiguration PowerShell.7.* | Unregister-PSSessionConfiguration
+
+$PluginSplat = @{
+    Path = $PSHOME.Replace($Env:ProgramFiles, '%ProgramFiles%')
+    ChildPath = 'pwrshplugin.dll'
+}
+$WSManPluginPath = Join-Path @PluginSplat
+
+<# $WSManSelection = @{
+    ResourceURI = 'winrm/config/plugin'
+    SelectorSet = @{ Name = 'PowerShell.7' }
+}
+Set-WSManInstance @WSManSelection -ValueSet = @{ Filename = $WSManPluginPath } #>
+
+$BasePath = 'WSMan:\localhost\Plugin\PowerShell.7'
+Set-Item -Path "$BasePath\FileName" -Value $WSManPluginPath
+Set-Item -Path "$BasePath\InitializationParameters\PSVersion" -Value 7
