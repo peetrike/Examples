48656f32a59b653e8a6d8131a903ba3d469639d1
Peter Wawa <meelisn@outlook.com>
2025-06-18 16:08:53 +0300

    add new sample quirk script

---
+++ src/quirks/PSResourceGet/Update-PSInfo.ps1
@@ -0,0 +1,20 @@
+#Requires -Version 5.1
+#Requires -Modules Microsoft.PowerShell.PSResourceGet
+#Requires -RunAsAdministrator
+
+<#PSScriptInfo
+    .VERSION 1.0.0.0
+    .GUID 3a7b2c5b-8ecf-4f45-9990-d9a3a8b9c4c3
+    .AUTHOR cpg4285
+#>
+
+<#
+    .SYNOPSIS
+        Testing Update-PSScriptFileInfo cmdlet
+    .DESCRIPTION
+        Script for testing Update-PSScriptFileInfo cmdlet
+
+        Additional paragraph.
+#>
+
+Write-Output 'Just testing'
