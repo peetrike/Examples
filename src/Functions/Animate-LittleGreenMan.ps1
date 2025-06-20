0207136d04acda9e7e727020a1ffd7f39ac324c3
Peter Wawa <meelisn@outlook.com>
2025-06-20 11:58:12 +0300

    add animation function

---
+++ src/Functions/Animate-LittleGreenMan.ps1
@@ -0,0 +1,44 @@
+﻿<#
+    .LINK
+        https://github.com/VernAnderson/PowerShell/blob/master/Animate-LittleGreenMan.ps1
+#>
+
+
+param (
+        [int]
+    $Steps,
+        [int]
+    $Interval
+)
+
+function Animate-LittleGreenMan {
+    param (
+            [int]
+        $Steps = 31,
+            [int]
+        $Interval = 125
+    )
+
+    $Head = 'O'
+
+    foreach ($Step in 0..$Steps) {
+        Clear-Host
+        if ($Step % 2 -eq 0) {
+            $Arms = '↙|↘'
+            $Legs = '/ \'
+            $BodyPad = $Step + 1
+        } else {
+            $Arms = '↓'
+            $Legs = '‖'
+            $BodyPad = $Step
+        }
+
+        Write-Host -Object $Head.PadLeft($Step) -ForegroundColor Green
+        Write-Host -Object $Arms.PadLeft($BodyPad) -ForegroundColor Green
+        Write-Host -Object $Legs.PadLeft($BodyPad) -ForegroundColor Green
+
+        Start-Sleep -Milliseconds $Interval
+    }
+}
+
+Animate-LittleGreenMan @PSBoundParameters
