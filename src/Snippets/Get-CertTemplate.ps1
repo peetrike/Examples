7b5b2c9c4c9478bb788332ff90931a03e9b53a46
Peter Wawa <meelisn@outlook.com>
2025-07-25 09:12:53 +0300

    add certificate template name extraction script

---
+++ src/Snippets/Get-CertTemplate.ps1
@@ -0,0 +1,43 @@
+﻿# Get-CertificateTemplates.ps1
+# Retrieves certificate templates used in the local machine's certificate store
+
+# Function to get certificate template information
+function Add-CertificateTemplateInfo {
+    param (
+            [Parameter(
+                Mandatory,
+                ValueFromPipeline
+            )]
+            [System.Security.Cryptography.X509Certificates.X509Certificate2]
+        $Certificate
+    )
+
+    begin {
+        $TemplateOid = '1.3.6.1.4.1.311.21.7'
+    }
+
+    process {
+        # Get certificate template information from extensions
+        $TemplateInfo = $Certificate.Extensions | Where-Object { $_.Oid.Value -eq $TemplateOid }
+
+        if ($TemplateInfo) {
+            # Parse template information
+            $templateString = $TemplateInfo.Format($false)
+            $templateName = if ($templateString -match '(Template|Mall)=([^,]+)') {
+                $matches[2].Trim()
+            } else { 'Unknown' }
+            $templateOID = if ($templateString -match 'OID=([0-9\.]+)') { $matches[1] } else { 'Unknown' }
+
+            $Certificate |
+                Add-Member -MemberType NoteProperty -Name TemplateName -Value $templateName -PassThru |
+                Add-Member -MemberType NoteProperty -Name TemplateOID -Value $templateOID -PassThru
+        } else {
+            $Certificate
+        }
+    }
+}
+
+# Get all certificates from the Local Machine store
+Get-ChildItem -Path Cert:\LocalMachine\My |
+    Add-CertificateTemplateInfo |
+    Select-Object -Property Thumbprint, Subject, Issuer, NotBefore, NotAfter, TemplateName, TemplateOID
