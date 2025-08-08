8a32a32b71147a8b09e22b6f77d149b91267bb7c
Peter Wawa <meelisn@outlook.com>
2025-08-08 10:09:18 +0300

    Add example of platyPS MAML conversion

---
+++ src/quirks/platyPS/MarkdownConversion.ps1
@@ -0,0 +1,23 @@
+﻿$ModuleName = 'SayHello'
+New-Module -Name $ModuleName -ScriptBlock {
+    function get-hello {
+        <#
+            .SYNOPSIS
+                Says hello
+            .DESCRIPTION
+                some examples for platyPS
+
+                - regular text
+                - text having **bold** in it
+        #>
+        'Hello'
+    }
+} | Import-Module
+
+$myModule = get-module $ModuleName
+
+New-MarkdownCommandHelp -ModuleInfo $myModule -OutputFolder $PSScriptRoot
+
+Get-ChildItem -Path $PSScriptRoot\$myModule\*.md |
+    Import-MarkdownCommandHelp |
+    Export-MamlCommandHelp -OutputFolder $PSScriptRoot
