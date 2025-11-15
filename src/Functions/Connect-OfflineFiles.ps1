8424bb1d3d1fc160dc7d7ca0097476ac123ff7a3
Peter Wawa <meelisn@outlook.com>
2025-11-15 11:26:25 +0200

    add new function

---
+++ src/Functions/Connect-OfflineFiles.ps1
@@ -0,0 +1,31 @@
+﻿function Connect-OfflineFiles {
+    <#
+        .SYNOPSIS
+            Connects to the Offline Files network location.
+        .DESCRIPTION
+            This function brings Offline Files location to online state.
+        .EXAMPLE
+            Connect-OfflineFiles -path \\server\share\path
+
+            This command brings the specified Offline Files location online.
+    #>
+    [CmdletBinding()]
+    param (
+            [ValidateScript({
+                Test-Path $_ -PathType Container
+            })]
+            [string]
+        $Path
+    )
+
+    $invokeCimMethodSplat = @{
+        ClassName  = 'Win32_OfflineFilesCache'
+        MethodName = 'TransitionOnline'
+        Arguments  = @{
+            Path  = $Path
+            Flags = [UInt32] 1
+        }
+    }
+
+    Invoke-CimMethod @invokeCimMethodSplat
+}
