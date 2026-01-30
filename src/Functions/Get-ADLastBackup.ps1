7fa2fc3aeaafbdb376b6ee7e98c93eabda1156ed
Peter Wawa <meelisn@outlook.com>
2026-01-30 11:46:42 +0200

    add new function

---
+++ src/Functions/Get-ADLastBackup.ps1
@@ -0,0 +1,43 @@
+﻿function Get-ADLastBackup {
+    [CmdletBinding()]
+    param (
+            [string]
+        $Domain,
+            [ValidateNotNull()]
+            [Management.Automation.PSCredential]
+            [Management.Automation.Credential()]
+        $Credential = [Management.Automation.PSCredential]::Empty
+    )
+
+    $DomainObject = if ($Domain) {
+        $ContextProps = @(
+            [DirectoryServices.ActiveDirectory.DirectoryContextType]::Domain
+            $Domain
+        )
+        if ($Credential -ne [Management.Automation.PSCredential]::Empty) {
+            Write-Verbose -Message ('Authenticating as {1} to connect to {0}' -f $Domain, $Credential.UserName)
+            $ContextProps += @(
+                $Credential.UserName
+                $Credential.GetNetworkCredential().Password
+            )
+        }
+        $context = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext -ArgumentList $ContextProps
+        [DirectoryServices.ActiveDirectory.Domain]::GetDomain($context)
+    } else {
+        [DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+    }
+
+    $DC = $DomainObject.FindDomainController()
+    $RootDSE = [adsi] ('LDAP://{0}/RootDSE' -f $DomainObject.Name)
+    $Context = $RootDSE.namingContexts | ForEach-Object { $_ }
+
+    foreach ($ctx in $Context) {
+        $dsaSignature = $DC.GetReplicationMetadata($ctx)['dsaSignature']
+        [PSCustomObject] @{
+            NamingContext = $ctx
+            DC            = $DC.Name
+            OriginatingDC = $dsaSignature.OriginatingServer
+            LastBackup    = $dsaSignature.LastOriginatingChangeTime
+        }
+    }
+}
---
+++ src/Functions/Get-ErrorMessage.ps1
@@ -0,0 +1,20 @@
+﻿function Get-ErrorMessage {
+    <#
+    .SYNOPSIS
+        Retrieves the error message corresponding to a given Win32 error code.
+    .DESCRIPTION
+        This function uses the WSMan.Automation COM object to fetch the
+        error message associated with a specified Win32 error code.
+    #>
+    param (
+            [Parameter(Mandatory = $true)]
+            [int]
+            # The Win32 error code for which to retrieve the error message.
+        $ErrorCode
+    )
+
+    $WsMan = New-Object -ComObject WSMan.Automation
+
+    $ConvertedCode = $ErrorCode -band 0xFFFF
+    $wsman.GetErrorMessage($ConvertedCode)
+}
