4d508d7fa7e4e14a24e450836343d65544a71263
Peter Wawa <meelisn@outlook.com>
2026-03-09 13:27:45 +0200

    add 2 new scripts

---
+++ src/Performance/Registry key value.benchmark.ps1
@@ -0,0 +1,54 @@
+﻿[CmdletBinding()]
+param (
+        [int]
+    $Min = 100,
+        [int]
+    $Max = 1000
+)
+
+$Technique = @{
+    'Select generic'    = {
+        $PropertyName = 'ProgramFilesDir'
+        $KeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+        Get-ItemProperty -Path $KeyPath |
+            Select-Object -ExpandProperty $PropertyName
+    }
+    'Select specific'   = {
+        $PropertyName = 'ProgramFilesDir'
+        $KeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+        Get-ItemProperty -Path $KeyPath -Name $PropertyName |
+            Select-Object -ExpandProperty $PropertyName
+    }
+    'Property generic'  = {
+        $PropertyName = 'ProgramFilesDir'
+        $KeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+        (Get-ItemProperty -Path $KeyPath).$PropertyName
+   }
+    'Property specific' = {
+        $PropertyName = 'ProgramFilesDir'
+        $KeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+        (Get-ItemProperty -Path $KeyPath -Name $PropertyName).$PropertyName
+   }
+}
+
+if ($PSVersionTable.PSVersion.Major -gt 2) {
+    if ($PSVersionTable.PSVersion.Major -gt 5) {
+        $Technique += @{
+            'PS 5 style' = {
+                $PropertyName = 'ProgramFilesDir'
+                $KeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion'
+                Get-ItemPropertyValue -Path $KeyPath -Name $PropertyName
+            }
+        }
+    }
+    for ($iterations = $Min; $iterations -le $Max; $iterations *= 10) {
+        Measure-Benchmark -RepeatCount $iterations -Technique $Technique -GroupName $iterations
+    }
+} else {
+    Write-Verbose -Message 'PowerShell 2'
+    Import-Module .\measure.psm1
+
+    foreach ($key in $Technique.Keys) {
+        Measure-ScriptBlock -method $key -Iterations $Max -ScriptBlock $Technique[$key]
+    }
+}
---
+++ src/Scripts/Test-PipeLineBinding.ps1
@@ -0,0 +1,28 @@
+﻿[CmdletBinding()]
+param (
+        [Parameter(
+            ValueFromPipeline
+        )]
+        [string]
+    $InputObject
+)
+
+begin {
+    Write-Verbose -Message 'Starting script'
+}
+
+<# process {
+        # You can't have process block when You excpect $input to contain values in end block
+    Write-Verbose -Message ('Processing input')
+    #$InputObject
+} #>
+
+end {
+    Write-Verbose -Message 'Ending script'
+    Write-Information -InformationAction Continue -MessageData 'InputObject values'
+    $InputObject
+
+    if ($DebugPreference) { Wait-Debugger }
+    Write-Information -InformationAction Continue -MessageData 'Input values'
+    $input
+}
