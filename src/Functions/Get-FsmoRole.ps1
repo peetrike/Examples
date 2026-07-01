e1ae3a5150c10ec6a6951320b8d914e3532fe86f
Peter Wawa <meelisn@outlook.com>
2026-07-01 11:46:14 +0300

    add new function

---
+++ src/Functions/Get-FsmoRole.ps1
@@ -0,0 +1,26 @@
+﻿param (
+        [Parameter(Position = 1)]
+        [string[]]
+    $Role = '*'
+)
+
+function Get-FsmoRole {
+    param (
+            [Parameter(Position = 1)]
+            [string[]]
+        $Role = '*'
+    )
+    $ForestRole = Get-ADForest | Select-Object *master
+    $DomainRole = Get-ADDomain | Select-Object PDC*, *master
+
+    $OutputProps = @{}
+    foreach ($r in $ForestRole.psobject.Properties.Name) {
+        $OutputProps[$r] = $ForestRole.$r
+    }
+    foreach ($r in $DomainRole.psobject.Properties.Name) {
+        $OutputProps[$r] = $DomainRole.$r
+    }
+    [PSCustomObject] $OutputProps | Select-Object -Property $Role
+}
+
+Get-FsmoRole @PSBoundParameters
