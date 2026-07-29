58d0838a4c8591f53bf2f66cf05642a3597506ea
Peter Wawa <meelisn@outlook.com>
2026-07-29 08:23:47 +0300

    Add Test-ScriptName script
    Extract script name using function and call stack

---
+++ src/Scripts/Test-ScriptName.ps1
@@ -0,0 +1,21 @@
+﻿
+New-Module -name Automation {
+    function Get-ScriptName {
+        [CmdletBinding()]
+        param ()
+
+        $namePattern = '\.ps1$'
+        $StackItem = Get-PSCallStack | Where-Object Command -Match $namePattern
+        $StackItem.Command -replace $namePattern
+    }
+
+    function Get-ScriptInfo {
+        [CmdletBinding()]
+        param ()
+
+        Get-ScriptName
+    }
+    Export-ModuleMember Get-ScriptInfo
+} | Import-Module
+
+Get-ScriptInfo
