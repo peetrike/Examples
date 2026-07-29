3731f1c54beb6a451e783b14b3a8822fbbda3d61
Peter Wawa <meelisn@outlook.com>
2026-07-29 08:20:50 +0300

    add automatic parameter test script
    see how to pass switch parameters

---
+++ src/Scripts/Test-AutoParameter.ps1
@@ -0,0 +1,24 @@
+﻿[CmdletBinding(
+    SupportsShouldProcess
+)]
+param ()
+
+function test {
+    [CmdletBinding(
+        SupportsShouldProcess
+    )]
+    param ()
+    [PSCustomObject]@{
+        Verbose = $PSBoundParameters['Verbose']
+        Debug   = $PSBoundParameters.ContainsKey('Debug')
+        WhatIf  = $PSBoundParameters['WhatIf']
+        Confirm = $PSBoundParameters['Confirm']
+    }
+}
+
+Write-Warning -Message ('Verbose is {0}' -f $PSBoundParameters['Verbose'])
+if ($PSBoundParameters['Whatif']) {
+    Write-Warning -Message 'Whatif present, skipping'
+} else {
+    test @PSBoundParameters
+}
