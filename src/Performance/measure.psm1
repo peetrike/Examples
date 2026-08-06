93a3576f4185f8a68fe1664bdee29ccc356734ef
Peter Wawa <meelisn@outlook.com>
2026-08-06 08:43:21 +0300

    Update PS2 measuring module
    - add -Technique support
    - add grouping support
    - add aliases to make module usable like BenchPress module substitution

--- src/Performance/measure.psm1
+++ src/Performance/measure.psm1
@@ -1,28 +1,61 @@
 ﻿function Measure-ScriptBlock {
     param (
+
+            [Parameter(Mandatory = $true)]
+            [Collections.IDictionary]
+        $Technique,
+            [Alias('RepeatCount')]
             [int]
         $Iterations = 10000,
             [string]
-        $Method,
-            [scriptblock]
-        $ScriptBlock
+        $GroupName
     )
 
-    $Type = @{
-        Name       = 'Method'
-        Expression = { $Method }
-    }
+    $ScriptName = Get-ScriptName
 
-    $Value = @{
-        Name       = 'Time'
-        Expression = { $_.ToString() }
+    $ThroughPut = @{
+        Name       = 'ThroughPut'
+        Expression = { $_.ThroughPut }
+        Format     = '{0:N2}/s'
     }
-    $Throughput = @{
-        Name       = 'Throughput'
-        Expression = { '{0:N2}/s' -f ($Iterations / $_.TotalSeconds) }
-    }
+    $KeyCount = $Technique.Keys.Count
+    $TechniqueCounter = 0
 
-    1..$Iterations |
-        Measure-Command $ScriptBlock |
-        Select-Object -Property $Type, $Value, $Throughput
+    $Technique.Keys | ForEach-Object {
+        $TechniqueCounter++
+        $percentComplete = 100 * $TechniqueCounter / $KeyCount
+        $method = $_
+
+        $progressSplat = @{
+            Activity        = "Timing $method"
+            Status          = "Over $Iterations repetitions"
+            PercentComplete = $percentComplete
+        }
+        Write-Progress @progressSplat
+
+        $TimeSpent = 1..$Iterations |
+            Measure-Command $Technique.$method
+
+        New-Object -TypeName psobject -Property @{
+            Method     = $method
+            Time       = $TimeSpent
+            ThroughPut = $Iterations / $TimeSpent.TotalSeconds
+            ScriptName = $ScriptName
+            Benchmark  = @($ScriptName, $GroupName) -join ' - '
+        }
+    } |
+        Sort-Object -Property Time |
+        Format-Table -AutoSize -Property Method, Time, $ThroughPut -GroupBy Benchmark
 }
+
+function Get-ScriptName {
+    [CmdletBinding()]
+    param ()
+
+    $namePattern = '(\.benchmark)?\.ps1$'
+    $StackItem = Get-PSCallStack | Where-Object { $_.ScriptName -match $namePattern } | Select-Object -First 1
+    (Split-Path -Path $StackItem.ScriptName -Leaf) -replace $namePattern
+}
+
+Set-Alias -Name Measure-Benchmark -Value Measure-ScriptBlock
+Export-ModuleMember -Function Measure-ScriptBlock -Alias Measure-Benchmark
