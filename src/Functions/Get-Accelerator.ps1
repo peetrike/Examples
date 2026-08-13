a8cf035e7dbb3110b42c9efba4b07958bdc246ee
Peter Wawa <meelisn@outlook.com>
2026-08-13 16:01:17 +0300

    Fix PS2 compatibility

--- src/Functions/Get-Accelerator.ps1
+++ src/Functions/Get-Accelerator.ps1
@@ -4,13 +4,13 @@
 param (
         [SupportsWildcards()]
         [string]
-    $Name = '*'
+    $Name
 )
 
 function Get-Accelerator {
     [CmdletBinding()]
     param (
-            [SupportsWildcards()]
+            #[SupportsWildcards()]
             [string]
         $Name = '*'
     )
@@ -18,9 +18,9 @@ function Get-Accelerator {
     $TAType = [psobject].Assembly.GetType('System.Management.Automation.TypeAccelerators')
     $AcceleratorList = $TAType::Get
     $AcceleratorList.Keys | Where-Object { $_ -like $Name } | ForEach-Object {
-        $PropList = [ordered] @{
+        $PropList = <# [ordered] #> @{
             Name = $_
-            Type = if ($_ -like 'psobject') { [psobject] } else { $AcceleratorList.$_ }
+            Type = if ($_ -like 'psobject') { [psobject] } else { $AcceleratorList[$_] }
         }
         New-Object -TypeName PSObject -Property $PropList
     }
