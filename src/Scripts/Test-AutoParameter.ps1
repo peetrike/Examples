51aca175cadc1247ba4ec957a8dff07282fd4ff2
Peter Wawa <meelisn@outlook.com>
2026-08-17 12:50:21 +0300

    add simple switch parameter to test

--- src/Scripts/Test-AutoParameter.ps1
+++ src/Scripts/Test-AutoParameter.ps1
@@ -4,18 +4,26 @@
         functions from same script, but not in functions from custom module.
 
         This script shows, how these parameters can be passed, when needed
+    .LINK
+
 #>
 
 [CmdletBinding(
     SupportsShouldProcess
 )]
-param ()
+param (
+        [switch]
+    $Test
+)
 
 function test {
     [CmdletBinding(
         SupportsShouldProcess
     )]
-    param ()
+    param (
+            [switch]
+        $Test
+    )
 
     if ($DebugPreference -eq 'Inquire') {
         $DebugPreference = 'Continue'
@@ -25,14 +33,16 @@ function test {
     Write-Debug -Message 'debug message'
 
     [PSCustomObject]@{
-        Verbose = [bool] $PSBoundParameters['Verbose']
-        Debug   = $PSBoundParameters.ContainsKey('Debug')
+        Verbose = [bool] $PSBoundParameters['Verbose']      # $VerbosePreference is [ActionPreference]
+        Debug   = $PSBoundParameters.ContainsKey('Debug')   # doesn't check for explicit false
         WhatIf  = $WhatIfPreference
-        Confirm = $Confirm.IsPresent
+        Confirm = $Confirm.IsPresent                        # doesn't work at all
+        Test    = $Test
     }
 }
 
-Write-Warning -Message ('Verbose is {0}' -f $PSBoundParameters['Verbose'])
-if ($PSCmdlet.ShouldProcess('run function')) {
+Write-Warning -Message ('Verbose is {0}' -f $Verbose.IsPresent)
+if ($PSCmdlet.ShouldProcess('test', 'run function')) {
     test @PSBoundParameters
+    Write-Host 'Parameters to be passed' "`n" $PSBoundParameters "`n"
 }
